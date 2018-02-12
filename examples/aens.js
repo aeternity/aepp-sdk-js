8006da7046cdbeb8a64c7eaa1ba70136e4d87edc
Till Kolter <till.kolter@gmail.com>
2018-02-12 18:35:25 +0100

    Clean AENS example

--- examples/aens.js
+++ examples/aens.js
@@ -3,57 +3,55 @@ require('@babel/polyfill')
 
 const EpochHtmlClient = require('../index')
 
-let clientAccount2 = new EpochHtmlClient('localhost', 3023, 3123, null, false)
-
-let localClient1 = new EpochHtmlClient('localhost', 3013, 3113, null, false)
-let localClient3 = new EpochHtmlClient('localhost', 3033, 3133, null, false)
-
-const ACCOUNT1 = 'ak$3iABijXLcdH17i6EDv84qkVKacKHCVwkGJ1bC9svzmfWmHxmXkD6qCeY6ewp4QJHjE4tMjuNstBobq8PXcZfg6vmz8fijM'
-const ACCOUNT2 = 'ak$3XpVuN8nVHmgSCtZGXtpzkiS2DufNDjWs9PdY2wxhDtMfeAGfJp8GfDMhH7rou2B2RVWcNhQtrVFNsRdvDsecsrLYHyuA2'
-const ACCOUNT3 = 'ak$3Xz1iHhC3wbySq1K4B6pEXjT1rbi5p6CzCta5FjxDMCUiMPaWyPXhnUqGu7HqqwanbYqwhCVpL9WCCFGqSP6ngMgwth3Nd'
+let client1 = new EpochHtmlClient('localhost', 3013, 3113, null, false)
+let client2 = new EpochHtmlClient('localhost', 3023, 3123, null, false)
+let client3 = new EpochHtmlClient('localhost', 3033, 3133, null, false)
 
 const aensLifecycle = async (domain) => {
-  let claimedDomain = await clientAccount2.aens.query(domain)
+  // get account pubkeys
+  let account1 = await client1.account.getPublicKey()
+  let account3 = await client3.account.getPublicKey()
+
+  let claimedDomain = await client2.aens.query(domain)
 
   let nameHash
   if (claimedDomain) {
     nameHash = claimedDomain['name_hash']
     console.log(`${domain} has already been registered: ${JSON.stringify(claimedDomain)}`)
   } else {
-    nameHash = await clientAccount2.aens.fullClaim(domain, 1, 1)
+    nameHash = await client2.aens.fullClaim(domain, 1, 1)
   }
 
-  let updatedNameHash = await clientAccount2.aens.update(ACCOUNT3, nameHash)
+  let updatedNameHash = await client2.aens.update(account3, nameHash)
   console.log(`${updatedNameHash} has been updated!`)
-  await clientAccount2.base.waitNBlocks(1)
+  await client2.base.waitNBlocks(1)
 
-  let aensData = await clientAccount2.aens.query(domain)
+  let aensData = await client2.aens.query(domain)
   if (aensData) {
     console.log(`Updated AENS ${JSON.stringify(aensData)}`)
   }
 
-  let balance1 = await localClient1.account.balance()
-  let balance3 = await localClient3.account.balance()
+  let balance1 = await client1.account.balance()
+  let balance3 = await client3.account.balance()
   console.log(`Current balances: AK 1 ${balance1}, AK3 ${balance3}`)
-  let success = await localClient1.base.spend(domain, 1, 1)
+  let success = await client1.base.spend(domain, 1, 1)
   console.log(`Account 1 sent ${success} token to Domain of Account 3!`)
-  await clientAccount2.base.waitNBlocks(1)
+  await client2.base.waitNBlocks(1)
 
-  balance1 = await localClient1.account.balance()
-  balance3 = await localClient3.account.balance()
+  balance1 = await client1.account.balance()
+  balance3 = await client3.account.balance()
   console.log(`Balances after transfer: AK1 ${balance1}, AK3 ${balance3}`)
 
 
-  await clientAccount2.aens.transfer(nameHash, ACCOUNT1, 1)
-  await clientAccount2.base.waitNBlocks(1)
-  let transferedData = await clientAccount2.aens.query(domain)
+  await client2.aens.transfer(nameHash, account1, 1)
+  await client2.base.waitNBlocks(1)
+  let transferedData = await client2.aens.query(domain)
   if (transferedData) {
     console.log(`Domain data now has pointer address ${JSON.parse(transferedData.pointers)['account_pubkey']}`)
   }
 
-  await clientAccount2.aens.revoke(nameHash, 1)
-  // The
-  await clientAccount2.base.waitNBlocks(1)
+  await client2.aens.revoke(nameHash, 1)
+  await client2.base.waitNBlocks(1)
 
   return true
 }
