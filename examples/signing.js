e9fefd54b611172706e637a598b8797c418b0d6d
Till Kolter <till.kolter@gmail.com>
2018-02-26 17:13:18 +0100

    Add key and private parameters

--- examples/signing.js
+++ examples/signing.js
@@ -17,20 +17,26 @@
  *  PERFORMANCE OF THIS SOFTWARE.
  */
 
-require('@babel/polyfill')
+require ('@babel/polyfill')
 
-const AeternityClient = require('../lib/aepp-sdk')
-const HttpProvider = require('../lib/providers/http')
+const AeternityClient = require ('../lib/aepp-sdk')
+const HttpProvider = require ('../lib/providers/http')
 
 
-let client1 = new AeternityClient(new HttpProvider('localhost', 3013, {internalPort: 3113, secured: false}))
-let client2 = new AeternityClient(new HttpProvider('localhost', 3023, {internalPort: 3123, secured: false}))
+let client1 = new AeternityClient (new HttpProvider ('localhost', 3013, {
+  internalPort: 3113,
+  secured: false
+}))
+let client2 = new AeternityClient (new HttpProvider ('localhost', 3023, {
+  internalPort: 3123,
+  secured: false
+}))
 
 
 const program = require ('commander')
-const prompt = require('prompt')
-const fs = require('fs')
-const Crypto = require('../lib/utils/crypto')
+const prompt = require ('prompt')
+const fs = require ('fs')
+const Crypto = require ('../lib/utils/crypto')
 
 let promptSchema = {
   properties: {
@@ -41,86 +47,109 @@ let promptSchema = {
       required: true,
       replace: '*',
       conform: function (value) {
-        return true;
+        return true
       }
     }
   }
 }
 
-const signTransaction = (amount, dir) => {
-  prompt.start()
-  prompt.get(promptSchema, async function(err, result) {
-    let password = result.password
+const signTransactionInternal = async function (recipient, amount, privateKey) {
 
-    console.log(`Decrypt the password secured keys`)
+  let data = await client1.base.getSpendTx (recipient, amount)
 
-    let key = fs.readFileSync(`${dir}/key`)
-    let pubKey = fs.readFileSync(`${dir}/key.pub`)
-    let decrypted = Crypto.decryptPrivateKey(password, key)
-    let privateHex = Buffer.from(decrypted).toString('hex')
-    let decryptedPub = Crypto.decryptPubKey(password, pubKey)
+  console.log (`\nCreate an unsigned spend transaction: ${JSON.stringify (data)}`)
 
-    console.log(`Private key (hex): ${privateHex}`)
-    console.log(`Public key (base check): ak\$${Crypto.encodeBase58Check(decryptedPub)}`)
+  let tx = data.tx
+  console.log ('\nGet byte buffer from hex private key')
+  // Get binary from hex variant of the private key
+  let binaryKey = Buffer.from (privateKey, 'hex')
 
-    let recipient = await client2.accounts.getPublicKey()
-
-    let data = await client1.base.getSpendTx(recipient, amount)
-
-    console.log(`\nCreate an unsigned spend transaction: ${JSON.stringify(data)}`)
-
-    let tx = data.tx
-    console.log('\nGet byte buffer from hex private key')
-    // Get binary from hex variant of the private key
-    let binaryKey = Buffer.from(privateHex, 'hex')
-
-    // Split the base58Check part of the transaction
-    let base58CheckTx = tx.split('$')[1]
-    let binaryTx = Crypto.decodeBase58Check(base58CheckTx)
-    console.log('\nSplit the tx hash after the $ and decode that base58check encoded string')
+  // Split the base58Check part of the transaction
+  let base58CheckTx = tx.split ('$')[1]
+  let binaryTx = Crypto.decodeBase58Check (base58CheckTx)
+  console.log ('\nSplit the tx hash after the $ and decode that base58check encoded string')
 
 
-    console.log('\nUse the ECDSA curve with \'secp256k1\' to sign the binary transaction with the binary private key')
-    let signature = Crypto.sign(binaryTx, binaryKey)
-    let sigBuffer = Buffer.from(signature)
-    console.log('\nThe signature as a byte buffer ' + JSON.stringify(sigBuffer))
+  console.log ('\nUse the ECDSA curve with \'secp256k1\' to sign the binary transaction with the binary private key')
+  let signature = Crypto.sign (binaryTx, binaryKey)
+  let sigBuffer = Buffer.from (signature)
+  console.log ('\nThe signature as a byte buffer ' + JSON.stringify (sigBuffer))
 
-    // the signed tx deserializer expects a 4-tuple:
-    // <tx_type, version, tx_dict, signatures_array>
-    let decodedTx = Crypto.decodeTx(tx)
-    console.log(`\nThe decoded tx looks like this: ${JSON.stringify(decodedTx)}`)
+  // the signed tx deserializer expects a 4-tuple:
+  // <tx_type, version, tx_dict, signatures_array>
+  let decodedTx = Crypto.decodeTx (tx)
+  console.log (`\nThe decoded tx looks like this: ${JSON.stringify (decodedTx)}`)
 
-    let unpackedSignedTx = [
-      Buffer.from('sig_tx'),
-      1,
-      decodedTx,
-      [sigBuffer]
-    ]
+  let unpackedSignedTx = [
+    Buffer.from ('sig_tx'),
+    1,
+    decodedTx,
+    [sigBuffer]
+  ]
 
-    console.log('\nPack the signed transaction as a 4-tuple <t, v, tx, sigs> with')
-    console.log('t -> Transaction Type (always "sig_tx")')
-    console.log('v -> Version')
-    console.log('tx -> A list of <key, value> pairs')
-    console.log('sigs -> A list of signatures')
+  console.log ('\nPack the signed transaction as a 4-tuple <t, v, tx, sigs> with')
+  console.log ('t -> Transaction Type (always "sig_tx")')
+  console.log ('v -> Version')
+  console.log ('tx -> A list of <key, value> pairs')
+  console.log ('sigs -> A list of signatures')
 
-    let signedTx = Crypto.encodeTx(unpackedSignedTx)
-    console.log(`\nThe signed base58check encoded and prefixed signed transaction:\n${signedTx}`)
+  let signedTx = Crypto.encodeTx (unpackedSignedTx)
+  console.log (`\nThe signed base58check encoded and prefixed signed transaction:\n${signedTx}`)
 
-    console.log('\nSend off the signed transaction')
-    await client1.tx.send(signedTx)
-    console.log('\nWait for 1 block')
-    await client1.base.waitNBlocks(1)
+  console.log ('\nSend off the signed transaction')
+  await client1.tx.send (signedTx)
+  console.log ('\nWait for 1 block')
+  await client1.base.waitNBlocks (1)
 
-    let transactions = await client1.accounts.getTransactions({txTypes: ['aec_spend_tx']})
-    console.log('Your recent spending transactions: ' + JSON.stringify(transactions))
-  })
+  let transactions = await client1.accounts.getTransactions ({txTypes: ['aec_spend_tx']})
+  console.log ('Your recent spending transactions: ' + JSON.stringify (transactions))
 
+}
+
+const signTransaction = async function (recipient, amount, options) {
+  if (!options || (!options.keys && !options.private)) {
+    throw 'Either --keys or --private has to be set'
+  }
+
+  // console.log (`${JSON.stringify (options)}`)
+  amount = parseInt(amount)
+
+  let privateHex
+  if (!options.private) {
+    let dir = options['keys']
+    console.log('kets ' + dir)
+    prompt.start ()
+    prompt.get (promptSchema, async function (err, result) {
+      let password = result.password
+
+      console.log (`Decrypt the password secured keys`)
+
+      let key = fs.readFileSync (`${dir}/key`)
+      let pubKey = fs.readFileSync (`${dir}/key.pub`)
+      let decrypted = Crypto.decryptPrivateKey (password, key)
+      privateHex = Buffer.from (decrypted).toString ('hex')
+      let decryptedPub = Crypto.decryptPubKey (password, pubKey)
+
+      console.log (`Private key (hex): ${privateHex}`)
+      console.log (`Public key (base check): ak\$${Crypto.encodeBase58Check (decryptedPub)}`)
+
+      // let recipient = await client2.accounts.getPublicKey ()
+      await signTransactionInternal(recipient, amount, privateHex)
+    })
+  } else {
+    privateHex = options.private
+    // let recipient = await client2.accounts.getPublicKey ()
+    await signTransactionInternal(recipient, amount, privateHex)
+
+  }
 }
 
 program
   .version ('0.1.0')
-  .command ('spend-signed <amount> <key_directory>')
+  .command ('spend-signed <recipient> <amount>')
   .description ('Decrypts public and private key to readable formats for testing purposes')
+  .option ('-d, --keys <keys>', 'Keys directory')
+  .option ('-k, --private <private>', 'Private key')
   .action (signTransaction)
 
 
