19a33d068f9f655a984d731695b9eb2507564b43
Denis Davidyuk <Denis_Davidyuk@hotmail.com>
2018-05-29 09:40:57 +0200

    Extract hex to buffer converter to utils

--- test/crypto.js
+++ test/crypto.js
@@ -18,18 +18,16 @@
 import { describe, it } from 'mocha'
 import { assert, expect } from 'chai'
 import Crypto from '../src/utils/crypto'
+import { hex } from './utils'
 
 // These keys are fixations for the encryption lifecycle tests and will
 // not be used for signing
-const privateKeyAsHex = '4d881dd1917036cc231f9881a0db978c8899dd76a817252418606b02bf6ab9d22378f892b7cc82c2d2739e994ec9953aa36461f1eb5a4a49a5b0de17b3d23ae8'
-const privateKey = Buffer.from(privateKeyAsHex, 'hex')
+const privateKey = hex`4d881dd1917036cc231f9881a0db978c8899dd76a817252418606b02bf6ab9d22378f892b7cc82c2d2739e994ec9953aa36461f1eb5a4a49a5b0de17b3d23ae8`
 const publicKeyWithPrefix = 'ak$Gd6iMVsoonGuTF8LeswwDDN2NF5wYHAoTRtzwdEcfS32LWoxm'
 const publicKey = Buffer.from(Crypto.decodeBase58Check(publicKeyWithPrefix.split('$')[1]))
 
-const txBinaryAsArray = [248, 76, 12, 1, 160, 35, 120, 248, 146, 183, 204, 130, 194, 210, 115, 158, 153, 78, 201, 149, 58, 163, 100, 97, 241, 235, 90, 74, 73, 165, 176, 222, 23, 179, 210, 58, 232, 160, 63, 40, 35, 12, 40, 65, 38, 215, 218, 236, 136, 133, 42, 120, 160, 179, 18, 191, 241, 162, 198, 203, 209, 173, 89, 136, 202, 211, 158, 59, 12, 122, 1, 1, 1, 132, 84, 101, 115, 116]
-const txBinary = Buffer.from(txBinaryAsArray)
-const signatureAsArray = [95, 146, 31, 37, 95, 194, 36, 76, 58, 49, 167, 156, 127, 131, 142, 248, 25, 121, 139, 109, 59, 243, 203, 205, 16, 172, 115, 143, 254, 236, 33, 4, 43, 46, 16, 190, 46, 46, 140, 166, 76, 39, 249, 54, 38, 27, 93, 159, 58, 148, 67, 198, 81, 206, 106, 237, 91, 131, 27, 14, 143, 178, 130, 2]
-const signature = Buffer.from(signatureAsArray)
+const txBinary = hex`f84c0c01a02378f892b7cc82c2d2739e994ec9953aa36461f1eb5a4a49a5b0de17b3d23ae8a03f28230c284126d7daec88852a78a0b312bff1a2c6cbd1ad5988cad39e3b0c7a0101018454657374`
+const signature = hex`5f921f255fc2244c3a31a79c7f838ef819798b6d3bf3cbcd10ac738ffeec21042b2e10be2e2e8ca64c27f936261b5d9f3a9443c651ce6aed5b831b0e8fb28202`
 
 describe('crypto', () => {
   describe('generateKeyPair', () => {
@@ -89,12 +87,10 @@ describe('crypto', () => {
 
   describe('personal messages', () => {
     const message = 'test'
-    const messageSignatureAsHex = '0d969f938571d1436106af659bc1893e7efeda685d7f096be39d0f2c68f712e4e9ba7551df31350a4a6d07943fbfc1cbe0acfe8271cd2dcdaac3274168e53901'
-    const messageSignature = Buffer.from(messageSignatureAsHex, 'hex')
+    const messageSignature = hex`0d969f938571d1436106af659bc1893e7efeda685d7f096be39d0f2c68f712e4e9ba7551df31350a4a6d07943fbfc1cbe0acfe8271cd2dcdaac3274168e53901`
 
     const messageNonASCII = 'tæst'
-    const messageNonASCIISignatureAsHex = '0aad557b424b33f4a2e415339d7ee143dea1f00c4a7c6c8680e1f70ff21e42b17e0e3729420daaed962999e089a18d099d39dc4a5ab4598d2b062106b59f1d01'
-    const messageNonASCIISignature = Buffer.from(messageNonASCIISignatureAsHex, 'hex')
+    const messageNonASCIISignature = hex`0aad557b424b33f4a2e415339d7ee143dea1f00c4a7c6c8680e1f70ff21e42b17e0e3729420daaed962999e089a18d099d39dc4a5ab4598d2b062106b59f1d01`
 
     describe('sign', () => {
       it('should produce correct signature of message', () => {
@@ -122,19 +118,18 @@ describe('crypto', () => {
   })
 
   describe('hd wallet', () => {
-    const h = ([hexString]) => Buffer.from(hexString, 'hex')
     const testMnemonic = 'eye quarter chapter suit cruel scrub verify stuff volume control learn dust'
     const testPassword = 'test-password'
     const testSaveHDWallet = {
-      chainCode: h`dd5cb572e8bddab36882ebbf87854e3b66f565447f20cfac874a5d3d7dd6d0d5`,
-      privateKey: h`731b6d83ff699d992959cd7f728285e07456a3589c4f1aac774158a63d9181fe`
+      chainCode: hex`dd5cb572e8bddab36882ebbf87854e3b66f565447f20cfac874a5d3d7dd6d0d5`,
+      privateKey: hex`731b6d83ff699d992959cd7f728285e07456a3589c4f1aac774158a63d9181fe`
     }
     const testAccounts = [{
-      publicKey: h`f75e53f57822227a58b463095d6dab657cab804574be62de0be1f95279d09037`,
-      secretKey: h`87abcb9c765f3259cf448542cae4c2e9bbff2ad2588693239fd7ca00b17fd463f75e53f57822227a58b463095d6dab657cab804574be62de0be1f95279d09037`
+      publicKey: hex`f75e53f57822227a58b463095d6dab657cab804574be62de0be1f95279d09037`,
+      secretKey: hex`87abcb9c765f3259cf448542cae4c2e9bbff2ad2588693239fd7ca00b17fd463f75e53f57822227a58b463095d6dab657cab804574be62de0be1f95279d09037`
     }, {
-      publicKey: h`1d7ce9bcf06e93c844c02489862b623c23b14a7364350a36336c87bc76b6650a`,
-      secretKey: h`e78fdb3c2600a0684906adfcb5fac33167576dcb099580bde000bc5a363c939c1d7ce9bcf06e93c844c02489862b623c23b14a7364350a36336c87bc76b6650a`
+      publicKey: hex`1d7ce9bcf06e93c844c02489862b623c23b14a7364350a36336c87bc76b6650a`,
+      secretKey: hex`e78fdb3c2600a0684906adfcb5fac33167576dcb099580bde000bc5a363c939c1d7ce9bcf06e93c844c02489862b623c23b14a7364350a36336c87bc76b6650a`
     }]
 
     describe('generateSaveHDWallet', () =>
--- test/hd-key.js
+++ test/hd-key.js
@@ -24,19 +24,19 @@ import {
   derivePathFromKey,
   derivePathFromSeed
 } from '../src/utils/hd-key'
+import { hex } from './utils'
 
 describe('hd-key', () => {
-  const h = ([hexString]) => Buffer.from(hexString, 'hex')
-  const testSeed = h`coffee`
+  const testSeed = hex`coffee`
   const testMasterKey = {
-    privateKey: h`5af2c70ffa0910605a26e651fc041de7c8c5a096fbb4bdaff2c597f692e2c145`,
-    chainCode: h`33d93dbe5e720d399e642222497579164d3dbf8c3f28e2147ecff96afad35d32`
+    privateKey: hex`5af2c70ffa0910605a26e651fc041de7c8c5a096fbb4bdaff2c597f692e2c145`,
+    chainCode: hex`33d93dbe5e720d399e642222497579164d3dbf8c3f28e2147ecff96afad35d32`
   }
   const testChildIndex = 0x80000000
   const testChildPath = 'm/0h'
   const testChildKey = {
-    privateKey: h`23a15775578d7bb4828189ebcb220240b30f11d3b1aadf28dc54db071ca325bf`,
-    chainCode: h`1fa9899b84631aaadf1daba7f7cf780e2fecfb8dd34a2edf6568eb1a9bb2f627`
+    privateKey: hex`23a15775578d7bb4828189ebcb220240b30f11d3b1aadf28dc54db071ca325bf`,
+    chainCode: hex`1fa9899b84631aaadf1daba7f7cf780e2fecfb8dd34a2edf6568eb1a9bb2f627`
   }
 
   describe('getMasterKeyFromSeed', () =>
@@ -65,70 +65,70 @@ describe('hd-key', () => {
 
   describe('check on test vectors for ed25519 from SLIP-0010', () =>
     [{
-      seed: h`000102030405060708090a0b0c0d0e0f`,
+      seed: hex`000102030405060708090a0b0c0d0e0f`,
       tests: [{
         path: 'm',
-        chain: h`90046a93de5380a72b5e45010748567d5ea02bbf6522f979e05c0d8d8ca9fffb`,
-        prv: h`2b4be7f19ee27bbf30c667b642d5f4aa69fd169872f8fc3059c08ebae2eb19e7`,
-        pub: h`00a4b2856bfec510abab89753fac1ac0e1112364e7d250545963f135f2a33188ed`
+        chain: hex`90046a93de5380a72b5e45010748567d5ea02bbf6522f979e05c0d8d8ca9fffb`,
+        prv: hex`2b4be7f19ee27bbf30c667b642d5f4aa69fd169872f8fc3059c08ebae2eb19e7`,
+        pub: hex`00a4b2856bfec510abab89753fac1ac0e1112364e7d250545963f135f2a33188ed`
       }, {
         path: 'm/0H',
-        chain: h`8b59aa11380b624e81507a27fedda59fea6d0b779a778918a2fd3590e16e9c69`,
-        prv: h`68e0fe46dfb67e368c75379acec591dad19df3cde26e63b93a8e704f1dade7a3`,
-        pub: h`008c8a13df77a28f3445213a0f432fde644acaa215fc72dcdf300d5efaa85d350c`
+        chain: hex`8b59aa11380b624e81507a27fedda59fea6d0b779a778918a2fd3590e16e9c69`,
+        prv: hex`68e0fe46dfb67e368c75379acec591dad19df3cde26e63b93a8e704f1dade7a3`,
+        pub: hex`008c8a13df77a28f3445213a0f432fde644acaa215fc72dcdf300d5efaa85d350c`
       }, {
         path: 'm/0H/1H',
-        chain: h`a320425f77d1b5c2505a6b1b27382b37368ee640e3557c315416801243552f14`,
-        prv: h`b1d0bad404bf35da785a64ca1ac54b2617211d2777696fbffaf208f746ae84f2`,
-        pub: h`001932a5270f335bed617d5b935c80aedb1a35bd9fc1e31acafd5372c30f5c1187`
+        chain: hex`a320425f77d1b5c2505a6b1b27382b37368ee640e3557c315416801243552f14`,
+        prv: hex`b1d0bad404bf35da785a64ca1ac54b2617211d2777696fbffaf208f746ae84f2`,
+        pub: hex`001932a5270f335bed617d5b935c80aedb1a35bd9fc1e31acafd5372c30f5c1187`
       }, {
         path: 'm/0H/1H/2H',
-        chain: h`2e69929e00b5ab250f49c3fb1c12f252de4fed2c1db88387094a0f8c4c9ccd6c`,
-        prv: h`92a5b23c0b8a99e37d07df3fb9966917f5d06e02ddbd909c7e184371463e9fc9`,
-        pub: h`00ae98736566d30ed0e9d2f4486a64bc95740d89c7db33f52121f8ea8f76ff0fc1`
+        chain: hex`2e69929e00b5ab250f49c3fb1c12f252de4fed2c1db88387094a0f8c4c9ccd6c`,
+        prv: hex`92a5b23c0b8a99e37d07df3fb9966917f5d06e02ddbd909c7e184371463e9fc9`,
+        pub: hex`00ae98736566d30ed0e9d2f4486a64bc95740d89c7db33f52121f8ea8f76ff0fc1`
       }, {
         path: 'm/0H/1H/2H/2H',
-        chain: h`8f6d87f93d750e0efccda017d662a1b31a266e4a6f5993b15f5c1f07f74dd5cc`,
-        prv: h`30d1dc7e5fc04c31219ab25a27ae00b50f6fd66622f6e9c913253d6511d1e662`,
-        pub: h`008abae2d66361c879b900d204ad2cc4984fa2aa344dd7ddc46007329ac76c429c`
+        chain: hex`8f6d87f93d750e0efccda017d662a1b31a266e4a6f5993b15f5c1f07f74dd5cc`,
+        prv: hex`30d1dc7e5fc04c31219ab25a27ae00b50f6fd66622f6e9c913253d6511d1e662`,
+        pub: hex`008abae2d66361c879b900d204ad2cc4984fa2aa344dd7ddc46007329ac76c429c`
       }, {
         path: 'm/0H/1H/2H/2H/1000000000H',
-        chain: h`68789923a0cac2cd5a29172a475fe9e0fb14cd6adb5ad98a3fa70333e7afa230`,
-        prv: h`8f94d394a8e8fd6b1bc2f3f49f5c47e385281d5c17e65324b0f62483e37e8793`,
-        pub: h`003c24da049451555d51a7014a37337aa4e12d41e485abccfa46b47dfb2af54b7a`
+        chain: hex`68789923a0cac2cd5a29172a475fe9e0fb14cd6adb5ad98a3fa70333e7afa230`,
+        prv: hex`8f94d394a8e8fd6b1bc2f3f49f5c47e385281d5c17e65324b0f62483e37e8793`,
+        pub: hex`003c24da049451555d51a7014a37337aa4e12d41e485abccfa46b47dfb2af54b7a`
       }]
     }, {
-      seed: h`fffcf9f6f3f0edeae7e4e1dedbd8d5d2cfccc9c6c3c0bdbab7b4b1aeaba8a5a29f9c999693908d8a8784817e7b7875726f6c696663605d5a5754514e4b484542`,
+      seed: hex`fffcf9f6f3f0edeae7e4e1dedbd8d5d2cfccc9c6c3c0bdbab7b4b1aeaba8a5a29f9c999693908d8a8784817e7b7875726f6c696663605d5a5754514e4b484542`,
       tests: [{
         path: 'm',
-        chain: h`ef70a74db9c3a5af931b5fe73ed8e1a53464133654fd55e7a66f8570b8e33c3b`,
-        prv: h`171cb88b1b3c1db25add599712e36245d75bc65a1a5c9e18d76f9f2b1eab4012`,
-        pub: h`008fe9693f8fa62a4305a140b9764c5ee01e455963744fe18204b4fb948249308a`
+        chain: hex`ef70a74db9c3a5af931b5fe73ed8e1a53464133654fd55e7a66f8570b8e33c3b`,
+        prv: hex`171cb88b1b3c1db25add599712e36245d75bc65a1a5c9e18d76f9f2b1eab4012`,
+        pub: hex`008fe9693f8fa62a4305a140b9764c5ee01e455963744fe18204b4fb948249308a`
       }, {
         path: 'm/0H',
-        chain: h`0b78a3226f915c082bf118f83618a618ab6dec793752624cbeb622acb562862d`,
-        prv: h`1559eb2bbec5790b0c65d8693e4d0875b1747f4970ae8b650486ed7470845635`,
-        pub: h`0086fab68dcb57aa196c77c5f264f215a112c22a912c10d123b0d03c3c28ef1037`
+        chain: hex`0b78a3226f915c082bf118f83618a618ab6dec793752624cbeb622acb562862d`,
+        prv: hex`1559eb2bbec5790b0c65d8693e4d0875b1747f4970ae8b650486ed7470845635`,
+        pub: hex`0086fab68dcb57aa196c77c5f264f215a112c22a912c10d123b0d03c3c28ef1037`
       }, {
         path: 'm/0H/2147483647H',
-        chain: h`138f0b2551bcafeca6ff2aa88ba8ed0ed8de070841f0c4ef0165df8181eaad7f`,
-        prv: h`ea4f5bfe8694d8bb74b7b59404632fd5968b774ed545e810de9c32a4fb4192f4`,
-        pub: h`005ba3b9ac6e90e83effcd25ac4e58a1365a9e35a3d3ae5eb07b9e4d90bcf7506d`
+        chain: hex`138f0b2551bcafeca6ff2aa88ba8ed0ed8de070841f0c4ef0165df8181eaad7f`,
+        prv: hex`ea4f5bfe8694d8bb74b7b59404632fd5968b774ed545e810de9c32a4fb4192f4`,
+        pub: hex`005ba3b9ac6e90e83effcd25ac4e58a1365a9e35a3d3ae5eb07b9e4d90bcf7506d`
       }, {
         path: 'm/0H/2147483647H/1H',
-        chain: h`73bd9fff1cfbde33a1b846c27085f711c0fe2d66fd32e139d3ebc28e5a4a6b90`,
-        prv: h`3757c7577170179c7868353ada796c839135b3d30554bbb74a4b1e4a5a58505c`,
-        pub: h`002e66aa57069c86cc18249aecf5cb5a9cebbfd6fadeab056254763874a9352b45`
+        chain: hex`73bd9fff1cfbde33a1b846c27085f711c0fe2d66fd32e139d3ebc28e5a4a6b90`,
+        prv: hex`3757c7577170179c7868353ada796c839135b3d30554bbb74a4b1e4a5a58505c`,
+        pub: hex`002e66aa57069c86cc18249aecf5cb5a9cebbfd6fadeab056254763874a9352b45`
       }, {
         path: 'm/0H/2147483647H/1H/2147483646H',
-        chain: h`0902fe8a29f9140480a00ef244bd183e8a13288e4412d8389d140aac1794825a`,
-        prv: h`5837736c89570de861ebc173b1086da4f505d4adb387c6a1b1342d5e4ac9ec72`,
-        pub: h`00e33c0f7d81d843c572275f287498e8d408654fdf0d1e065b84e2e6f157aab09b`
+        chain: hex`0902fe8a29f9140480a00ef244bd183e8a13288e4412d8389d140aac1794825a`,
+        prv: hex`5837736c89570de861ebc173b1086da4f505d4adb387c6a1b1342d5e4ac9ec72`,
+        pub: hex`00e33c0f7d81d843c572275f287498e8d408654fdf0d1e065b84e2e6f157aab09b`
       }, {
         path: 'm/0H/2147483647H/1H/2147483646H/2H',
-        chain: h`5d70af781f3a37b829f0d060924d5e960bdc02e85423494afc0b1a41bbe196d4`,
-        prv: h`551d333177df541ad876a60ea71f00447931c0a9da16f227c11ea080d7391b8d`,
-        pub: h`0047150c75db263559a70d5778bf36abbab30fb061ad69f69ece61a72b0cfa4fc0`
+        chain: hex`5d70af781f3a37b829f0d060924d5e960bdc02e85423494afc0b1a41bbe196d4`,
+        prv: hex`551d333177df541ad876a60ea71f00447931c0a9da16f227c11ea080d7391b8d`,
+        pub: hex`0047150c75db263559a70d5778bf36abbab30fb061ad69f69ece61a72b0cfa4fc0`
       }]
     }].forEach(({ seed, tests }, idx) =>
       describe(`vector ${idx + 1}`, () =>
