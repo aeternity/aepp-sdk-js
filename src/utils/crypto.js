56be2440720b4a68410354412316e57fa7774cf5
Alexander Kahl <ak@sodosopa.io>
2018-06-13 15:04:32 +0200

    Refactor examples and provide docco-based documentation

--- src/utils/crypto.js
+++ src/utils/crypto.js
@@ -146,3 +146,104 @@ export function isValidKeypair (privateKey, publicKey) {
   const signature = sign(message, privateKey)
   return verify(message, signature, publicKey)
 }
+
+export function envKeypair (env) {
+  const keypair = {
+    priv: env['WALLET_PRIV'],
+    pub: env['WALLET_PUB']
+  }
+
+  if (keypair.pub && keypair.priv) {
+    return keypair
+  } else {
+    throw Error('Environment variables WALLET_PRIV and WALLET_PUB need to be set')
+  }
+}
+
+export const decode = RLP.decode
+
+const OBJECT_TAGS = {
+  SIGNED_TX: 11,
+  CHANNEL_CREATE_TX: 50,
+  CHANNEL_CLOSE_MUTUAL_TX: 53,
+  CHANNEL_OFFCHAIN_TX: 57
+}
+
+function readInt (buf) {
+  return buf.readIntBE(0, buf.length)
+}
+
+function readSignatures (buf) {
+  const signatures = []
+
+  for (let i = 0; i < buf.length; i++) {
+    signatures.push(encodeBase58Check(buf[i]))
+  }
+
+  return signatures
+}
+
+function readOffChainTXUpdates (buf) {
+  const updates = []
+
+  for (let i = 0; i < buf.length; i++) {
+    updates.push([
+      'ak$' + encodeBase58Check(buf[i][0]),
+      'ak$' + encodeBase58Check(buf[i][1]),
+      readInt(buf[i][2])
+    ])
+  }
+
+  return updates
+}
+
+export function deserialize (binary) {
+  const obj = {
+    tag: readInt(binary[0]),
+    version: readInt(binary[1])
+  }
+
+  switch (obj.tag) {
+    case OBJECT_TAGS.SIGNED_TX:
+      return Object.assign(obj, {
+        signatures: readSignatures(binary[2]),
+        tx: deserialize(decode(binary[3]))
+      })
+
+    case OBJECT_TAGS.CHANNEL_CREATE_TX:
+      return Object.assign(obj, {
+        initiator: 'ak$' + encodeBase58Check(binary[2]),
+        initiatorAmount: readInt(binary[3]),
+        responder: 'ak$' + encodeBase58Check(binary[4]),
+        responderAmount: readInt(binary[5]),
+        channelReserve: readInt(binary[6]),
+        lockPeriod: readInt(binary[7]),
+        ttl: readInt(binary[8]),
+        fee: readInt(binary[9]),
+        nonce: readInt(binary[10])
+      })
+
+    case OBJECT_TAGS.CHANNEL_CLOSE_MUTUAL_TX:
+      return Object.assign(obj, {
+        channelId: encodeBase58Check(binary[2]),
+        initiatorAmount: readInt(binary[3]),
+        responderAmount: readInt(binary[4]),
+        ttl: readInt(binary[5]),
+        fee: readInt(binary[6]),
+        nonce: readInt(binary[7])
+      })
+
+    case OBJECT_TAGS.CHANNEL_OFFCHAIN_TX:
+      return Object.assign(obj, {
+        channelId: encodeBase58Check(binary[2]),
+        previousRound: readInt(binary[3]),
+        round: readInt(binary[4]),
+        initiator: 'ak$' + encodeBase58Check(binary[5]),
+        responder: 'ak$' + encodeBase58Check(binary[6]),
+        initiatorAmount: readInt(binary[7]),
+        responderAmount: readInt(binary[8]),
+        updates: readOffChainTXUpdates(binary[9]),
+        state: encodeBase58Check(binary[10])
+      })
+  }
+}
