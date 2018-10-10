06b6d259578e9421905029adca31b35fdf7e7e05
nduchak <nduchak@n-ix.com>
2018-10-10 17:11:42 +0300

    Update description

--- bin/aecli-contract.js
+++ bin/aecli-contract.js
@@ -33,6 +33,7 @@ program
   .option('--host [hostname]', 'Node to connect to', utils.constant.EPOCH_URL)
   .option('--internalUrl [internal]', 'Node to connect to(internal)', utils.constant.EPOCH_INTERNAL_URL)
   .option('-T, --ttl [ttl]', 'Validity of the transaction in number of blocks (default forever)', utils.constant.CONTRACT_TTL)
+  .option('-n, --nonce [nonce]', 'Override the nonce that the transaction is going to be sent with')
   .option('-f --force', 'Ignore epoch version compatibility check')
   .option('--json', 'Print result in json format')
 
@@ -45,7 +46,6 @@ program
 program
   .command('call <wallet_path> <desc_path> <fn> <return_type> [args...]')
   .option('-P, --password [password]', 'Wallet Password')
-  .option('-n, --nonce [nonce]', 'Transaction nonce')
   .option('-G --gas [gas]', 'Amount of gas to call the contract', utils.constant.GAS)
   .description('Execute a function of the contract')
   .action(async (walletPath, path, fn, returnType, args, ...arguments) => await Contract.call(walletPath, path, fn, returnType, args, utils.cli.getCmdFromArguments(arguments)))
@@ -55,7 +55,6 @@ program
   .option('-P, --password [password]', 'Wallet Password')
   .option('-I, --init [state]', 'Deploying contract arguments for constructor function')
   .option('-G --gas [gas]', 'Amount of gas to deploy the contract', utils.constant.GAS)
-  .option('-n, --nonce [nonce]', 'Transaction nonce')
   .description('Deploy a contract on the chain')
   .action(async (walletPath, path, ...arguments) => await Contract.deploy(walletPath, path, utils.cli.getCmdFromArguments(arguments)))
 
