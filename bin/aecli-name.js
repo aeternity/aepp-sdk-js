fabcec73fbeba0b72ad46132021b47c6130433bf
nduchak <nduchak@n-ix.com>
2018-10-10 18:55:15 +0300

    Add docs for CLI commander file's

--- bin/aecli-name.js
+++ bin/aecli-name.js
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
+// # æternity CLI `name` file
+//
+// This script initialize all `name` commands
 /*
  * ISC License (ISC)
  * Copyright (c) 2018 aeternity developers
@@ -15,20 +18,16 @@
  *  OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
  *  PERFORMANCE OF THIS SOFTWARE.
  */
-
-//  _   _
-// | \ | |
-// |  \| | __ _ _ __ ___   ___  ___
-// | . ` |/ _` | '_ ` _ \ / _ \/ __|
-// | |\  | (_| | | | | | |  __/\__ \
-// |_| \_|\__,_|_| |_| |_|\___||___/
-
+// We'll use `commander` for parsing options
+//
+// Also we need `esm` package to handle `ES imports`
 const program = require('commander')
 
 require = require('esm')(module/*, options*/) //use to handle es6 import/export
 const utils = require('./utils/index')
 const { AENS } = require('./commands')
 
+// ## Initialize `options`
 program
   .option('-H, --host [hostname]', 'Node to connect to', utils.constant.EPOCH_URL)
   .option('-U, --internalUrl [internal]', 'Node to connect to(internal)', utils.constant.EPOCH_INTERNAL_URL)
@@ -39,27 +38,53 @@ program
   .option('-n, --nonce [nonce]', 'Override the nonce that the transaction is going to be sent with')
   .option('--json [json]', 'Print result in json format')
 
+// ## Initialize `claim` command
+//
+// You can use this command to `claim` AENS name. Name must end on `.aet`.
+//
+// Example: `aecli name claim ./myWalletKeyFile --password testpass  testname.aet`
+//
+// This command send `pre-claim` transaction, wait until one block was mined, after that sent `claim` and `update` transaction's
+//
+// You can use `--nameTtl` and `--ttl` to pre-set transaction and name `time to leave`
 program
   .command('claim <wallet_path> <name>')
   .description('Claim a domain name')
   .action(async (walletPath, name, ...arguments) => await AENS.claim(walletPath, name, utils.cli.getCmdFromArguments(arguments)))
 
+// ## Initialize `revoke` command
+//
+// You can use this command to `destroy` AENS name.
+//
+// Example: `aecli name revoke ./myWalletKeyFile --password testpass testname.aet`
 program
   .command('revoke  <wallet_path> <name>')
-  .description('Claim a domain name')
+  .description('Revoke a domain name')
   .action(async (walletPath, name, ...arguments) => await AENS.revokeName(walletPath, name, utils.cli.getCmdFromArguments(arguments)))
 
+// ## Initialize `transfer` command
+//
+// You can use this command to `transfer` AENS name to another account.
+//
+// Example: `aecli name transfer ./myWalletKeyFile --password testpass testname.aet ak_qqwemjgflewgkj349gjdslksd`
 program
   .command('transfer <wallet_path> <name> <address>')
   .description('Transfer a name to another account')
   .action(async (walletPath, name, address, ...arguments) => await AENS.transferName(walletPath, name, address, utils.cli.getCmdFromArguments(arguments)))
 
+// ## Initialize `claim` command
+//
+// You can use this command to `update` pointer of AENS name.
+//
+// Example: `aecli name update ./myWalletKeyFile --password testpass testname.aet ak_qwe23dffasfgdesag323`
 program
   .command('update <wallet_path> <name> <address>')
   .description('Update a name pointer')
   .action(async (walletPath, name, address, ...arguments) => await AENS.updateName(walletPath, name, address, utils.cli.getCmdFromArguments(arguments)))
 
+// Handle unknown command's
 program.on('command:*', () => utils.errors.unknownCommandHandler(program)())
 
+// Parse arguments or show `help` if argument's is empty
 program.parse(process.argv)
 if (program.args.length === 0) program.help()
