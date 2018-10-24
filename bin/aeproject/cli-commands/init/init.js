1385e3e60b071e36a92dad29de92f778a8644c3c
Martin Grigorov <maptuhec@gmail.com>
2018-10-24 18:14:48 +0300

    Add ISC to JS files, rename some variables

--- bin/aeproject/cli-commands/init/init.js
+++ bin/aeproject/cli-commands/init/init.js
@@ -1,3 +1,20 @@
+/*
+ * ISC License (ISC)
+ * Copyright (c) 2018 aeternity developers
+ *
+ *  Permission to use, copy, modify, and/or distribute this software for any
+ *  purpose with or without fee is hereby granted, provided that the above
+ *  copyright notice and this permission notice appear in all copies.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+ *  REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+ *  AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+ *  INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+ *  LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+ *  OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *  PERFORMANCE OF THIS SOFTWARE.
+ */
+
 require = require('esm')(module /*, options */ ) // use to handle es6 import/export
 
 import {
@@ -20,7 +37,7 @@ const deployFileDestination = `${deployDir}/deploy.js`;
 const contractsDir = './contracts';
 const contractTemplateFile = 'Identity.aes';
 const contractFileDestination = `${contractsDir}/Identity.aes`;
-const artifactsDirectory = `${__dirname}/artifacts/`;
+const artifactsDir = `${__dirname}/artifacts/`;
 
 const dockerDir = './docker';
 const dockerTemplateDir = 'docker';
@@ -51,7 +68,7 @@ async function run() {
 }
 
 const installLibraries = async () => {
-  const fileSource = `${artifactsDirectory}/package.json`;
+  const fileSource = `${artifactsDir}/package.json`;
   utils.copyFileOrDir(fileSource, "./package.json")
   await installAeppSDK();
 }
@@ -74,35 +91,35 @@ const installAeppSDK = async () => {
 
 const setupContracts = () => {
   print(`===== Creating contracts directory =====`);
-  const fileSource = `${artifactsDirectory}/${contractTemplateFile}`;
+  const fileSource = `${artifactsDir}/${contractTemplateFile}`;
   utils.createIfExistsFolder(contractsDir);
   utils.copyFileOrDir(fileSource, contractFileDestination)
 }
 
 const setupTests = () => {
   print(`===== Creating tests directory =====`);
-  const fileSource = `${artifactsDirectory}/${testTemplateFile}`;
+  const fileSource = `${artifactsDir}/${testTemplateFile}`;
   utils.createIfExistsFolder(testDir, "Creating tests file structure");
   utils.copyFileOrDir(fileSource, testFileDestination)
 }
 
 const setupDeploy = () => {
   print(`===== Creating deploy directory =====`);
-  const fileSource = `${artifactsDirectory}/${deployTemplateFile}`;
+  const fileSource = `${artifactsDir}/${deployTemplateFile}`;
   utils.createIfExistsFolder(deployDir, "Creating deploy directory file structure");
   utils.copyFileOrDir(fileSource, deployFileDestination)
 }
 
 const setupDocker = () => {
   print(`===== Creating docker directory =====`);
-  const fileSourceDir = `${artifactsDirectory}/${dockerTemplateDir}`;
+  const dockerFilesSource = `${artifactsDir}/${dockerTemplateDir}`;
   const copyOptions = {
     overwrite: true
   }
 
-  const fileSourceYml = `${artifactsDirectory}/${dockerYmlFile}`;
-  utils.copyFileOrDir(fileSourceYml, dockerYmlFileDestination, copyOptions)
-  utils.copyFileOrDir(fileSourceDir, dockerFilesDestination, copyOptions)
+  const dockerYmlFileSource = `${artifactsDir}/${dockerYmlFile}`;
+  utils.copyFileOrDir(dockerYmlFileSource, dockerYmlFileDestination, copyOptions)
+  utils.copyFileOrDir(dockerFilesSource, dockerFilesDestination, copyOptions)
 }
 
 module.exports = {
