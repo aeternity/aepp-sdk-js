b835cd3b6b7822746d9c3812177e8d41fce3a059
Enrico Icardi <enrico.icardi@gmail.com>
2018-10-25 17:42:12 +0200

    bringing back .babelrc for consistency

--- examples/connect-two-ae/identity/babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  presets: ['@babel/preset-env'],
-  ignore: [/[/\\]core-js/, /@babel[/\\]runtime/],
-  plugins: [
-    '@babel/plugin-proposal-object-rest-spread',
-    '@babel/plugin-transform-runtime',
-    '@babel/plugin-proposal-export-default-from'
-  ]
-}
