8adc07fe4ee1886117bb4860a1c6eec32e708491
Martin Grigorov <maptuhec@gmail.com>
2018-11-06 15:27:40 +0200

    Change test config urls

--- bin/aeproject/cli-commands/init/artifacts/exampleTests.js
+++ bin/aeproject/cli-commands/init/artifacts/exampleTests.js
@@ -17,8 +17,8 @@
 const Ae = require('@aeternity/aepp-sdk').Universal;
 
 const config = {
-  host: "http://localhost:3001/",
-  internalHost: "http://localhost:3001/internal/",
+  host: "http://localhost:3013",
+  internalHost: "http://localhost:3113",
   ownerKeyPair: {
     secretKey: 'bb9f0b01c8c9553cfbaf7ef81a50f977b1326801ebf7294d1c2cbccdedf27476e9bbf604e611b5460a3b3999e9771b6f60417d73ce7c5519e12f7e127a1225ca',
     publicKey: 'ak_2mwRmUeYmfuW93ti9HMSUJzCk1EYcQEfikVSzgo6k2VghsWhgU'
