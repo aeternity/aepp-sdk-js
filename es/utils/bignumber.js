7d7ec637bb768e20f962415426713d8d5bd6220d
nduchak <nduchak@n-ix.com>
2018-11-13 18:02:46 +0200

    Fix build

--- es/utils/bignumber.js
+++ es/utils/bignumber.js
@@ -1,6 +1,5 @@
-import BigNumber from 'bignumber.js';
+import BigNumber from 'bignumber.js'
 
-export async function parseBigNumber(number) {
+export async function parseBigNumber (number) {
   return (new BigNumber(number)).toString(10)
 }
-
