284b7302d5f2c2de03dc8570868f6874636d261e
nduchak <nduchak@n-ix.com>
2018-11-23 12:27:21 +0200

    fix contract test

--- test/integration/transaction.js
+++ test/integration/transaction.js
@@ -39,7 +39,7 @@ const pointers = [{ key: 'account_pubkey', id: senderId }]
 const code = 'cb_Dmwce5QeGDTaXBnQKcZmHYte3DLx2kPCbZVvgCMRy3jcc64YVLnrinrfoirN58XxyibrJv4Ju9qY1bXPf3PCF5opkeGwr4ezCsJBc81NXNTm5v3bMfbWKa1pcYcCJBn7tsCgT9ZWUJrc7kGN46bhVNS42TUWpm3WHFBxVRsLeJKqVsc4qFafnZ57pNwYYpYeLENG9QVmE3FaNENhHs6pLuTFVegrDZfyPbZ9y7HSEyDhELo56moQm5gqAS5sCNG1qbDYd4a3UGiZgtDhYLVgrTRvCubmBJEQeWeFUviPLjuqU4NkSqnRQF8kxQMT5Cf42JguLM7umQu665NLzUSLqJoNaa2dBiLg1ubmaXy2UcJLxCuAbwUHkbgAjD2qBmyhWgPzmwnqptu6R764kYbjfyVoF2FvwaSmsJ6wAVjhsMLsk2EoAwWiyfpN412NU3q69Rrk2NgAg15VshWnH6tpSSMyd5ihMuoSWKNeHhmdoq4VdbiaUVVKbMWP99w7eAXYs7xYftcZ1Qmf5RyvPMNaVxzVF1T84uNbe2pToGJQrStHtEb7Crz8zHdyzQC8yZsSnBEY5jnFVJicBiLYGgyM25swCzVsh6BsJrjhWHjGyJ3Ei6Qq98ZX5Rry6qbYEqGPAEjEo2yyPj4DSggEhpp7bjhnk2rgjeZdQthNWKBNTppTNsJYVuNcrE95523fnQUP3zSGCrEJCzEuFs7qZxGgbcdY14ibwPdMmjv3R5iZP7wm7BR2GvGGE4fDYhcMLCecT6Z97f5zXu8j1Z4s1XFiRtLSHmkW1esm787kHoXMpaMCmbGzjNpUKhkSEsKwozRT2vSiuqjHQdiANpNucbBQMn2qz9yKyQGWqiDe9yTrJGoW8a7JajDUdy6KEi9jdHd8oA4s75tL6GB8maAecMHt47h7K9ZhtxFdFjSjR3qpy6UV9T6Henbm2DgCvUtqF9FG8YEDDnxs5d7Yb43jK6VYiA15Ggxd1iRMauWN9ckXctGVrdBfDga22WuJHK5WoahKodNs8rwouGjGQSa9eb5mWyrNRdfNdboYqa1J3bmj6LUhY92dvTMuUMQ8pF83bropWAfanFtnJorDkEiyzq6HcWkNiZk4LAECL8TtbKsQEU3VmgCpXuQSA7K6tudCpJt2iyekXb2kYX8Kvp1m1frrThgvVkBU4mBk5LZzJoj3KGgYn7zkLFG7kBPM64SjSotFCyzQHQkbRkfC6noPgZhdUeXBNsboRJGintac8ZC4CYC5PjW2E6YnQxjdmz1wf3DZh8LSJDfmKgHP8johYd45FVouAiAiLXmJtwgkxFuNjv21i6uQLqupvAaHbPB1cezUubBr49tqFo1NzeDwjV5tQc1RcYdr6PZBZcvYsXcuMdWH8Rhq4udRCWAibQzduUbx8CVWhetHMBLVRUmUZmgwcjotrTACe2M8ACKddGf9XMjL4JXjK4YzNzsaAngUjfWcDuFcX5xUJrXdAr3TR9YpkyfDHzoV3B5mSgN1gjFd4wowVZWTVEh3kuYw1YDKpm'
 const create_callData = 'cb_1111111111111111111111111111111Ku45JH6TECRXvrTEB2CJEWX1JDnpka1miFLNSeAqFxfV8cLGugsx3e3oDfXhKpp11WngHCwPrVASdgbKphcEz3cGXnc1sH'
 const call_callData = 'cb_11111111111111111111111111111112gRjTdsrTQXzZz62s7h7y8uKnzwGCdH9Sf336S27gMUWCQ7jkepZyWyUhTGuseoqsFTShSTkKJcCmoW9uZhdU9BahVSaQW1FYg6XhFctuLKdtunbukKBDQnHc2QGdT5BpSa3xRVwcHE2Er7Lna4k8NTUv19Ehk8p5B5BkZrLThBNaZFeTX1Y6Z'
-const contractId = 'ct_EGLNLZNMoHWtZswRT3jFcVb6sidVhhgWSMvPHSX43L8BatbVv'
+let contractId
 const vmVersion = 1
 const deposit = 4
 const gasPrice =  1
@@ -113,8 +113,12 @@ describe('Native Transaction', function () {
   })
 
   it.only('native build of contract create tx', async () => {
-    const txFromAPI = await client.contractCreateTx({ ownerId: senderId, code, vmVersion, deposit, amount, gas, gasPrice, fee, ttl, callData: create_callData, nonce: 5  })
-    const nativeTx = await clientNative.contractCreateTx({ ownerId: senderId, code, vmVersion, deposit, amount, gas, gasPrice, fee, ttl, callData: create_callData, nonce: 5  })
+    const txFromAPI = await client.contractCreateTx({ ownerId: senderId, code, vmVersion, deposit, amount, gas, gasPrice, fee, ttl, callData: create_callData  })
+    const nativeTx = await clientNative.contractCreateTx({ ownerId: senderId, code, vmVersion, deposit, amount, gas, gasPrice, fee, ttl, callData: create_callData })
+    // deploy contract
+    await client.send(txFromAPI.tx)
+    contractId = txFromAPI.contractId
+
     txFromAPI.tx.should.be.equal(nativeTx.tx)
     txFromAPI.contractId.should.be.equal(nativeTx.contractId)
   })
