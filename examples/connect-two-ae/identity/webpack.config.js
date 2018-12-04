083636fc24590d94e0bb38e50a73ccbea730ed9d
Enrico Icardi <enrico.icardi@gmail.com>
2018-12-04 18:06:09 +0100

    examples aepp-in-aepp

--- examples/connect-two-ae/identity/webpack.config.js
+++ examples/connect-two-ae/identity/webpack.config.js
@@ -25,7 +25,10 @@ module.exports = {
   mode: process.env.NODE_ENV === 'prod' ? 'production' : 'development',
   resolve: {
     alias: {
-      AE_SDK_MODULES: '@aeternity/aepp-sdk/es'
+      // use this if you are installing the SDK as dependency
+      // AE_SDK_MODULES: path.resolve(__dirname, 'node_modules/@aeternity/aepp-sdk/es/')
+      // use this, if you are running this app from inside the Aepp-SDK repo/folder
+      AE_SDK_MODULES: '../../../../../es/'
     }
   },
   entry: {
@@ -72,25 +75,10 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        loader: jsLoader,
-        // include: [path.resolve(__dirname, 'aepp'), path.resolve(__dirname, 'wallet')]
-        exclude: /node_modules/
-      },
-      {
-        test: /\.js$/,
-        include: [
-          path.resolve(__dirname, 'node_modules/@aeternity'),
-          path.resolve(__dirname, 'node_modules/rlp')
-        ],
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env'],
-          plugins: [
-            '@babel/plugin-proposal-object-rest-spread',
-            '@babel/plugin-transform-runtime',
-            '@babel/plugin-proposal-export-default-from'
-          ]
-        }
+        // exclude: /node_modules/,
+        exclude: [/node_modules/],
+        include: [/node_modules\/@aeternity/, /node_modules\/rlp/],
+        loader: 'babel-loader'
       },
       {
         test: /\.css$/,
