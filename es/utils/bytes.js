97087434bac61550286a8c4f2d4106476e769f22
Denis Davidyuk <denis_davidyuk@hotmail.com>
2018-12-24 11:45:57 +0100

    Simplify bigNumberToByteArray function

--- es/utils/bytes.js
+++ es/utils/bytes.js
@@ -58,11 +58,7 @@ export function rightPad (length, inputBuffer) {
 function bigNumberToByteArray (x) {
   let hexString = x.toString(16)
   if (hexString.length % 2 > 0) hexString = '0' + hexString
-  let byteArray = []
-  for (let i = 0; i < hexString.length; i += 2) {
-    byteArray.push(parseInt(hexString.slice(i, i + 2), 16))
-  }
-  return Buffer.from(byteArray)
+  return Buffer.from(hexString, 'hex')
 }
 
 export function toBytes (val, big = false) {
