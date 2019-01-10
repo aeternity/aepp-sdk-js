1c09ea569013f3bcaf59b963aef70c0631c5de4d
nduchak <nduchak@n-ix.com>
2019-01-10 14:00:21 +0200

    export keystore through bundle small refactor

--- es/index.js
+++ es/index.js
@@ -17,6 +17,7 @@
 
 import Ae from './ae'
 import * as Crypto from './utils/crypto'
+import * as KeyStore from './utils/keystore'
 import Chain from './chain'
 import EpochChain from './chain/epoch'
 import Tx from './tx'
@@ -38,6 +39,7 @@ export {
   Ae,
   Aepp,
   Crypto,
+  KeyStore,
   Chain,
   EpochChain,
   EpochContract,
