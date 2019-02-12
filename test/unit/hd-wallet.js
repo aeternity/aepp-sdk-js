ca248448d459dc926f4daaabfb9afbba80eb081d
nduchak <nduchak@n-ix.com>
2019-02-12 19:53:41 +0200

    fix build

--- test/unit/hd-wallet.js
+++ test/unit/hd-wallet.js
@@ -14,7 +14,6 @@
  *  OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
  *  PERFORMANCE OF THIS SOFTWARE.
  */
-
 import { expect } from 'chai'
 import { describe, it } from 'mocha'
 import {
