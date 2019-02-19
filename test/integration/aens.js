3831b3768805e8aeb1e262d14fa3479d07fde3af
nduchak <nduchak@n-ix.com>
2019-02-19 13:24:20 +0200

    fix aens test

--- test/integration/aens.js
+++ test/integration/aens.js
@@ -35,7 +35,7 @@ describe('Aens', function () {
 
   before(async function () {
     aens = await ready(this)
-    await aens.spend('124245235', account.publicKey)
+    await aens.spend('10000000000000', account.publicKey)
   })
 
   describe('fails on', () => {
