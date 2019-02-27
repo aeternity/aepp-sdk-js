ea471134da7006609034c414fd1f32a8de8c1a1f
nduchak <nduchak@n-ix.com>
2019-02-27 16:08:50 +0200

    Fix typo

--- test/integration/channel.js
+++ test/integration/channel.js
@@ -21,7 +21,7 @@ import { configure, ready, plan, BaseAe, networkId } from './'
 import { generateKeyPair } from '../../es/utils/crypto'
 import Channel from '../../es/channel'
 
-const wsUlr = process.env.WS_URL || 'ws://node:3014'
+const wsUrl = process.env.WS_URL || 'ws://node:3014'
 
 plan('10000000000000000')
 
@@ -53,7 +53,7 @@ describe('Channel', function () {
     return null
   }
   const sharedParams = {
-    url: wsUlr,
+    url: wsUrl,
     pushAmount: 3,
     initiatorAmount: 1000000000000000,
     responderAmount: 1000000000000000,
