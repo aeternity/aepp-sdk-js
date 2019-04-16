00d4a7318e50e02f6e59511db4e43b310b4592a4
nduchak <nduchak@n-ix.com>
2019-04-16 13:44:00 +0300

    build(Compiler): Add compiler to docker-compose

--- test/integration/index.js
+++ test/integration/index.js
@@ -21,7 +21,7 @@ import { BigNumber } from 'bignumber.js'
 
 const url = process.env.TEST_URL || 'http://localhost:3013'
 const internalUrl = process.env.TEST_INTERNAL_URL || 'http://localhost:3113'
-const compilerUrl = 'http://localhost:3007'
+const compilerUrl = 'http://localhost:3080'
 const networkId = process.env.TEST_NETWORK_ID || 'ae_devnet'
 const account = Crypto.generateKeyPair()
 
