cfdb47c038a49de51a938278e126e71e0911a82e
nduchak <nduchak@n-ix.com>
2019-04-16 15:39:59 +0300

    build(Compiler): Add compiler env to docker

--- test/integration/contract.js
+++ test/integration/contract.js
@@ -44,7 +44,7 @@ const encodedNumberSix = 'cb_AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaKNdnK'
 
 plan('10000000000000000')
 
-describe.only('Contract', function () {
+describe('Contract', function () {
   configure(this)
 
   let contract
