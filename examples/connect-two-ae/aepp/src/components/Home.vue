6feedf359e75e731f7ba836d3982332c1999c90d
Denis Davidyuk <denis_davidyuk@hotmail.com>
2019-04-18 07:02:11 +0200

    aepp example: Add spend with payload

--- examples/connect-two-ae/aepp/src/components/Home.vue
+++ examples/connect-two-ae/aepp/src/components/Home.vue
@@ -24,20 +24,66 @@
       </div>
     </div>
 
-    <h2 class="mt-4">Compile Contract</h2>
+    <h2 class="mt-4">Spend tokens</h2>
 
     <div class="border mt-4 rounded">
       <div class="bg-grey-lightest w-full flex flex-row font-mono">
         <div class="p-2 w-1/4">
-          Contract By
+          Recipient address
         </div>
-        <div v-if="pub" class="p-2 w-3/4 bg-white break-words">
-          {{pub}}
-        </div>
-        <div v-if="!pub" class="p-2 w-3/4 bg-grey-lightest break-words text-grey">
-          Requesting Public Key from AE Wallet...
+        <div class="p-2 w-3/4 bg-white break-words">
+          <input
+            class="bg-black text-white border-b border-black p-2 w-full"
+            v-model="spendTo"
+            placeholder="ak_..."
+          />
         </div>
       </div>
+      <div class="bg-grey-lightest w-full flex flex-row font-mono">
+        <div class="p-2 w-1/4">
+          Tokens amount
+        </div>
+        <div class="p-2 w-3/4 bg-white break-words">
+          <input
+            class="bg-black text-white border-b border-black p-2 w-full"
+            v-model="spendAmount"
+          />
+        </div>
+      </div>
+      <div class="bg-grey-lightest w-full flex flex-row font-mono">
+        <div class="p-2 w-1/4">
+          Payload
+        </div>
+        <div class="p-2 w-3/4 bg-white break-words">
+          <input
+            class="bg-black text-white border-b border-black p-2 w-full"
+            v-model="spendPayload"
+          />
+        </div>
+      </div>
+      <button
+        v-if="client"
+        class="w-32 rounded rounded-full bg-purple text-white py-2 px-4 pin-r mr-8 mt-4 text-xs"
+        @click="spend"
+      >
+        Spend
+      </button>
+    </div>
+
+    <div v-if="spendResult || spendError" class="border mt-4 mb-8 rounded">
+      <div class="bg-green w-full flex flex-row font-mono border border-b">
+        <div class="p-2 w-1/4">
+          Send result
+        </div>
+        <div
+          class="p-2 w-3/4 bg-grey-lightest break-words whitespace-pre-wrap"
+        >{{ spendResult ? JSON.stringify(spendResult, null, 4) : spendError }}</div>
+      </div>
+    </div>
+
+    <h2 class="mt-4">Compile Contract</h2>
+
+    <div class="border mt-4 rounded">
       <div class="bg-grey-lightest w-full flex flex-row font-mono">
         <div class="p-2 w-1/4">
           Contract Code
@@ -104,6 +150,11 @@ export default {
       client: null,
       height: null,
       pub: null,
+      spendTo: null,
+      spendAmount: null,
+      spendPayload: null,
+      spendResult: null,
+      spendError: null,
       contractCode: `contract Identity =
   type state = ()
   function main(x : int) = x`,
@@ -117,6 +168,18 @@ export default {
     }
   },
   methods: {
+    async spend () {
+      try {
+        this.spendResult = await this.client.spend(
+          this.spendAmount,
+          this.spendTo, {
+            payload: this.spendPayload,
+          }
+        )
+      } catch (err) {
+        this.spendError = err
+      }
+    },
     async compile () {
       this.byteCode = this.compileError = null
       try {
