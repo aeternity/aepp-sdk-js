219d3ebebe0c55238c464004e10a704b519b690b
nduchak <nduchak@n-ix.com>
2019-05-15 19:50:19 +0300

    fix(Fix semver implem,entation):

--- es/utils/semver-satisfies.js
+++ es/utils/semver-satisfies.js
@@ -1,11 +1,13 @@
 export default function (version, geVersion, ltVersion) {
-  const versionComponents = version.split('.')
+  const versionComponents = version.split('-')[0].split('.')
   const geComponents = geVersion.split('.')
   const ltComponents = ltVersion.split('.')
   const base = Math.max(...versionComponents, ...geComponents, ...ltComponents) + 1
   const toNumber = components => components.reverse()
     .reduce((acc, n, idx) => acc + n * Math.pow(base, idx), 0)
 
-  return toNumber(versionComponents) >= toNumber(geComponents) &&
-    toNumber(versionComponents) < toNumber(ltComponents)
+  const vNumber = toNumber(versionComponents)
+  const geNumber = toNumber(geComponents)
+  const ltNumber = toNumber(ltComponents)
+  return vNumber >= geNumber && vNumber < ltNumber
 }
