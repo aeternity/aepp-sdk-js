4ba7ae253e0fb389ef1a0a62d4759dbc556a5894
nduchak <nduchak@n-ix.com>
2019-06-13 11:58:43 +0300

    chore(test): Fix contract test

--- test/integration/contract.js
+++ test/integration/contract.js
@@ -151,7 +151,8 @@ describe('Contract', function () {
     })
     it('Use invalid compiler url', async () => {
       try {
-        await contract.setCompilerUrl('https://compiler.aepps.comas')
+        const cloned = Object.assign({}, contract)
+        await cloned.setCompilerUrl('https://compiler.aepps.comas')
       } catch (e) {
         e.message.should.be.equal('Compiler do not respond')
       }
