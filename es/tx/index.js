26beba8f48f04717abbcab35228518a19fc48b33
naz_dou <41945483+nduchak@users.noreply.github.com>
2019-08-28 16:17:41 +0300

    feat(GA): Disable GA (#631)
    * feat(GA): Disable GA
    
    * fix(Linter): linter error
    
    * fix(TX): Remove gaAttach from tx interface
    
    * chore(GA): Add todos to enable GA
    
    * fix(Compiler): Fix forceCompatibility issue
    
    * fix(Compiler): Tests
    
    * chore(Node): Adjust node compatibility range

--- es/tx/index.js
+++ es/tx/index.js
@@ -48,7 +48,9 @@ const Tx = stampit({
         'spendTx', 'namePreclaimTx', 'nameClaimTx', 'nameTransferTx',
         'nameUpdateTx', 'nameRevokeTx', 'contractCreateTx', 'contractCallTx',
         'oracleRegisterTx', 'oracleExtendTx', 'oraclePostQueryTx', 'oracleRespondTx', 'getAccountNonce',
-        'channelCloseSoloTx', 'channelSlashTx', 'channelSettleTx', 'channelSnapshotSoloTx', 'gaAttachTx', 'getVmVersion', 'prepareTxParams'
+        'channelCloseSoloTx', 'channelSlashTx', 'channelSettleTx', 'channelSnapshotSoloTx', 'getVmVersion', 'prepareTxParams'
+        // Todo Enable GA
+        // 'gaAttachTx',
       ]
     }
   }
@@ -71,7 +73,8 @@ const Tx = stampit({
     channelSlashTx: required,
     channelSettleTx: required,
     channelSnapshotSoloTx: required,
-    gaAttachTx: required,
+    // Todo Enable GA
+    // gaAttachTx: required,
     getVmVersion: required,
     prepareTxParams: required
   }
