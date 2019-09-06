c76b78bf2094b5bb7c16bf1efee165c5794f5e7e
naz_dou <omelko39@gmail.com>
2019-09-06 13:44:25 +0300

    feat(Lima): Make contact for channel test payable

--- test/integration/channel.js
+++ test/integration/channel.js
@@ -29,8 +29,8 @@ const wsUrl = process.env.TEST_WS_URL || 'ws://localhost:3014/channel'
 plan(BigNumber('1000e18').toString())
 
 const identityContract = `
-contract Identity =
-  entrypoint main(x : int): int = x
+payable contract Identity =
+  payable entrypoint main(x : int): int = x
 `
 
 function waitForChannel (channel) {
