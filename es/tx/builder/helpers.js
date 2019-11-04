67895a8b50391047d1cebf2e0d67928bfc77f7dd
naz_dou <omelko39@gmail.com>
2019-11-04 11:25:53 +0200

    fix(AENS): auction end block calculation

--- es/tx/builder/helpers.js
+++ es/tx/builder/helpers.js
@@ -297,9 +297,9 @@ export function computeBidFee (domain, startFee = NAME_FEE, increment = NAME_FEE
 
 export function computeAuctionEndBlock (domain, claimHeight) {
   return R.cond([
-    [R.lt(4), R.always(NAME_BID_TIMEOUTS[1] + claimHeight)],
-    [R.lt(8), R.always(NAME_BID_TIMEOUTS[4] + claimHeight)],
-    [R.lte(NAME_BID_MAX_LENGTH), R.always(NAME_BID_TIMEOUTS[8] + claimHeight)],
+    [R.lt(5), R.always(NAME_BID_TIMEOUTS[4] + claimHeight)],
+    [R.lt(9), R.always(NAME_BID_TIMEOUTS[8] + claimHeight)],
+    [R.lte(NAME_BID_MAX_LENGTH), R.always(NAME_BID_TIMEOUTS[12] + claimHeight)],
     [R.T, R.always(claimHeight)]
   ])(domain.replace('.chain', '').length)
 }
