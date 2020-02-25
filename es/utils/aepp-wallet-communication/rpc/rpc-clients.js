af2f3a022a24f0d30b1b2886d134d3c60d81fd0e
Denis Davidyuk <denis_davidyuk@hotmail.com>
2020-02-25 21:08:17 +1000

    RpcClient: Simplify message id processing (#916)
    Co-authored-by: naz_dou <41945483+nduchak@users.noreply.github.com>

--- es/utils/aepp-wallet-communication/rpc/rpc-clients.js
+++ es/utils/aepp-wallet-communication/rpc/rpc-clients.js
@@ -122,7 +122,6 @@ export const RpcClients = stampit({
  */
 export const RpcClient = stampit({
   init ({ id, name, networkId, icons, connection, handlers: [onMessage, onDisconnect] }) {
-    const messageId = 0
     this.id = id
     this.connection = connection
     this.info = { name, networkId, icons }
@@ -134,12 +133,12 @@ export const RpcClient = stampit({
     this.addressSubscription = []
     this.accounts = {}
 
-    this.sendMessage = sendMessage(messageId, this.connection)
+    this.sendMessage = sendMessage(this.connection)
     const disconnect = (aepp, connection) => {
       this.disconnect(true)
       typeof onDisconnect === 'function' && onDisconnect(connection, this)
     }
-    connection.connect(receive(onMessage, messageId), disconnect)
+    connection.connect(receive(onMessage), disconnect)
   },
   methods: {
     /**
