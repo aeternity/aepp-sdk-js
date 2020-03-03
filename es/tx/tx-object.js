6c1210128ec8cb03b8ce592d35fbdc4b2911a2b6
naz_dou <omelko39@gmail.com>
2020-03-03 18:15:20 +0200

    feat(Tx): Implement init of TxObject. Add validation

--- es/tx/tx-object.js
+++ es/tx/tx-object.js
@@ -4,28 +4,24 @@ import { isHex } from '../utils/string'
 import { buildTx, unpackTx } from './builder'
 import { TX_TYPE } from './builder/schema'
 
-const prepareTx = (tx, type) => {
-  if (!tx) throw new Error('Transaction is required!')
-  // Encoded Tx or raw tx as hex
+const buildTransaction = (type, params, options = {}) => {
+  if (typeof params !== 'object') throw new Error('"params" should be an object')
+  if (typeof type !== 'string' || !Object.values(TX_TYPE).includes(type)) throw new Error(`Unknown transaction type ${type}`)
+  return buildTx(params, type, options)
+}
+const unpackTransaction = (tx, options) => {
+  if (!tx) throw new Error(`Invalid transaction: ${tx}`)
   if (typeof tx === 'string') {
-    if (isHex(tx)) {
-      return unpackTx(Buffer.from(tx, 'hex'))
-    }
-    if (assertedType(tx, 'tx', true)) {
-      return unpackTx(tx)
-    }
-  }
-  if (typeof tx === 'object') {
-    if (!type || Object.prototype.hasOwnProperty.apply(TX_TYPE, type)) throw new Error(`Unknown transaction type ${type}`)
-    return buildTx(tx, type)
+    if (isHex(tx)) return unpackTx(Buffer.from(tx, 'hex'))
+    if (!assertedType(tx, 'tx', true)) throw new Error('Invalid transaction string. Tx should be `ak` prefixed base58c string')
+    return unpackTx(isHex(tx) ? Buffer.from(tx, 'hex') : tx, isHex(tx))
   }
 }
 
 export const TxObject = stampit({
-  init (tx, type) {
-    if (!tx) throw new Error('Invalid transaction')
-    const txData = prepareTx(tx, type)
-    return Object.assign(this, txData)
+  init ({ tx, params, type }) {
+    if (params && type) return Object.assign(this, buildTransaction(params, type))
+    if (tx) return Object.assign(this, unpackTransaction(tx))
   },
   methods: {
     recalculateFee: () => true,
