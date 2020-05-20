254a9409e618ae3001946aae6a7621e22bd8c706
naz_dou <omelko39@gmail.com>
2020-05-20 15:52:43 +0300

    feat(AEX_2): Fix RPC test's

--- examples/browser/extension/src/js/inject.js
+++ examples/browser/extension/src/js/inject.js
@@ -2,7 +2,7 @@ import BrowserRuntimeConnection
   from '@aeternity/aepp-sdk/es/utils/aepp-wallet-communication/connection/browser-runtime'
 import BrowserWindowMessageConnection
   from '@aeternity/aepp-sdk/es/utils/aepp-wallet-communication/connection/browser-window-message'
-import { getBrowserAPI, isInIframe } from '@aeternity/aepp-sdk/es/utils/aepp-wallet-communication/helpers'
+import { getBrowserAPI } from '@aeternity/aepp-sdk/es/utils/aepp-wallet-communication/helpers'
 import { MESSAGE_DIRECTION } from '@aeternity/aepp-sdk/es/utils/aepp-wallet-communication/schema'
 import ContentScriptBridge from '@aeternity/aepp-sdk/es/utils/aepp-wallet-communication/content-script-bridge'
 
