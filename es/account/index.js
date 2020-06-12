7eb405bfb9cb952ae184812ad6773dcd5cc7f91e
naz_dou <omelko39@gmail.com>
2020-06-12 15:59:17 +0300

    feat(Validator): Add ability to sign transaction hash instead of full transaction. Add `signHash=true`

--- es/account/index.js
+++ es/account/index.js
@@ -26,7 +26,7 @@ import stampit from '@stamp/it'
 import { required } from '@stamp/required'
 
 import { hash, personalMessageToBinary, decodeBase64Check, assertedType, verifyPersonalMessage } from '../utils/crypto'
-import { buildTx } from '../tx/builder'
+import { buildTx, buildTxHash } from '../tx/builder'
 import { decode } from '../tx/builder/helpers'
 import { TX_TYPE } from '../tx/builder/schema'
 import { getNetworkId } from '../node'
@@ -40,11 +40,11 @@ import { getNetworkId } from '../node'
  * @param {Object} opt - Options
  * @return {String} Signed transaction
  */
-async function signTransaction (tx, opt = {}) {
+async function signTransaction (tx, opt = { signHash: true }) {
   const networkId = this.getNetworkId(opt)
   const rlpBinaryTx = decodeBase64Check(assertedType(tx, 'tx'))
   // Prepend `NETWORK_ID` to begin of data binary
-  const txWithNetworkId = Buffer.concat([Buffer.from(networkId), rlpBinaryTx])
+  const txWithNetworkId = Buffer.concat([Buffer.from(networkId), opt.signHash ? buildTxHash(rlpBinaryTx) : rlpBinaryTx])
 
   const signatures = [await this.sign(txWithNetworkId, opt)]
   return buildTx({ encodedTx: rlpBinaryTx, signatures }, TX_TYPE.signed).tx
