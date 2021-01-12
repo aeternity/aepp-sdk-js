53953eb02742bef855807a670e784541c5810356
Igor Markin <markin.io210@gmail.com>
2021-01-12 16:16:53 +0300

    Update semver-satisfies.js
    A fix for problem with node's semantic version digits count

--- es/utils/semver-satisfies.js
+++ es/utils/semver-satisfies.js
@@ -3,11 +3,12 @@ export default function (version, geVersion, ltVersion) {
   const geComponents = geVersion.split('-')[0].split('.')
   const ltComponents = ltVersion.split('-')[0].split('.')
   const base = Math.max(...versionComponents, ...geComponents, ...ltComponents) + 1
-  const toNumber = components => components.reverse()
+  const toNumber = (components, sliceLength) => components.slice(0, sliceLength).reverse()
     .reduce((acc, n, idx) => acc + n * Math.pow(base, idx), 0)
 
-  const vNumber = toNumber(versionComponents)
-  const geNumber = toNumber(geComponents)
-  const ltNumber = toNumber(ltComponents)
+  const componentsMinAmount = Math.min(versionComponents.length, geComponents.length, ltComponents.length);
+  const vNumber = toNumber(versionComponents, componentsMinAmount)
+  const geNumber = toNumber(geComponents, componentsMinAmount)
+  const ltNumber = toNumber(ltComponents, componentsMinAmount)
   return vNumber >= geNumber && vNumber < ltNumber
 }
