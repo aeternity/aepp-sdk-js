cbc0bd6c569b7c196103f563a5e96a26d4a27847
Nikita Fuchs <q.fuchs@gmail.com>
2021-01-18 12:27:02 +0100

    before changing processor to add files to an array

--- process-docfiles.js
+++ process-docfiles.js
@@ -67,14 +67,15 @@ const assignDepth = (arr, depth = 1, parentFileName = "") => {
                 return {json: arr.content, content: undefined}
             }
              */
-            
+
             // if the depth is over 2, pass the previous parentFileName. 
             // If under, pass the key as parentFileName
             parentFile = depth > 2 ? parentFileName : key
             let {content, json} = assignDepth(arr[key], depth + 1, parentFile)
 
-            // "on the object we set object's key to..."
+            // "on the object we set object's key to the deeper stuff"
             arr[key] = json
+
             // store all the returned contents TODO: maybe sort keys alphabetically ?
             if (content != null) { 
                 contentAccumulator =  contentAccumulator + ' \n \n ' + content
@@ -85,9 +86,8 @@ const assignDepth = (arr, depth = 1, parentFileName = "") => {
         // add it to the object, but only if depth is at least two, because nesting 2 needs to contain all the rest of contents.
         depth > 1 ? arr.content = contentAccumulator : true
         //arr.content = contentAccumulator
-
-        // when done, we add the depth key to it.
         arr.depth = depth
+        // when done, we add the depth key to it.
         return {content: arr.content, json: arr}
     }
     
@@ -117,8 +117,104 @@ parsedYaml = YAML.load('mkdocs_original.yml');
 
 // assign depth to each nav entry
 let JSONwithDepth = assignDepth(parsedYaml.nav)
+fs.writeFileSync('./parsedYaml.nav.txt', JSON.stringify(JSONwithDepth, null, 2))
 
+// delete what's nested too deep (that stuff remains under the keys 'content)
+const deleteTooDeep = (arr, depth = 0, parentFileName = "") => {
+    
+    // we work on the input array, not constructing new one!
+    const isObject = A => {
+        if( (typeof A === "object" || typeof A === 'function') && (A !== null) )
+        {
+            return true
+        } else {return false}
+    }
+
+    // if it's an array:
+    if(Array.isArray(arr)){
+
+
+        arr.forEach((value, index, array) => {
+            let json = deleteTooDeep(value)
+            // remove the result if its depth is over 2 !
+            //console.log(value)
+            //arr[index = json]
+             if (isObject(json) && json.depth > 2){  
+                 console.log("Hier")
+                 console.log(json)
+                arr.splice(index,1)
+            } else {
+                // just in case if JSON is not an object, add it anyway.
+                arr[index] = json
+            } 
+            // <<<-------- accumulate content here !!!
+        })
+
+        return arr 
+        
+    }  
+    
+    // check if array value is an object:
+    else if (isObject(arr) == true) {
+        // if yes, we process all its keys
+
+        Object.keys(arr).forEach(key => {
+            // if the 'content' key is checked, return that one as JSON basically, and add its
+            // content to the accumulator so it's returned later, too
+            
+       /*      if (key == "content") {
+                console.log("checking content")
+                contentAccumulator = contentAccumulator + ' \n \n ' + arr.key
+                return {json: arr.content, content: undefined}
+            }
+             */
+
+            // if the depth is over 2, pass the previous parentFileName. 
+            // If under, pass the key as parentFileName
+            //parentFile = depth > 2 ? parentFileName : key
+            let json = deleteTooDeep(arr[key], depth + 1)
+
+            // "on the object we set object's key to the deeper stuff"
+            arr[key] = json
+
+            // store all the returned contents TODO: maybe sort keys alphabetically ?
+        })
+
+        // so if one of the keys returned content, 
+        // add it to the object, but only if depth is at least two, because nesting 2 needs to contain all the rest of contents.
+        //arr.content = contentAccumulator
+        arr.depth = arr.depth
+        // when done, we add the depth key to it.
+        return arr
+    }
+    
+    // here it's only string.
+     return arr
+                
+    
+    // we read the file's content, and return it. it gets combined in the array handler !
+    
+}
+
+// save the unflattened result
 console.log(JSONwithDepth)
 fs.writeFileSync('./testOutput.txt', JSON.stringify(JSONwithDepth, null, 2))
+
+console.log("Now flattened: ")
+const flattened = deleteTooDeep(JSONwithDepth)
+
+console.log(deleteTooDeep(flattened))
+fs.writeFileSync('./flattened.txt', JSON.stringify(flattened, null, 2))
 //console.log(JSON.stringify(JSONwithDepth, null, 2))
 
+const reFlattened = deleteTooDeep(flattened)
+
+fs.writeFileSync('./reFlattened.txt', JSON.stringify(reFlattened, null, 2))
+
+// another recursive function. this time, the TOCs array is populated with only newly constructed 
+// objects for its array
+
+
+
+// next: for every object: take the "content", make a file out of it, and attach it to the list of files 
+// in the array of that object (found under the key which is NOT depth and NOT content)
---
+++ site/hooks.py
@@ -0,0 +1,8 @@
+import os.path
+import subprocess
+
+def pre_build(**kwargs):
+  if not os.path.exists('node_modules'):
+    subprocess.run(['npm', 'install', '--ignore-scripts'], check=True)
+  subprocess.run(['npm', 'run', 'docs:docco'], check=True)
+  subprocess.run(['npm', 'run', 'docs:api'], check=True)
