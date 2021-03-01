5c3ef5d07d9d825905745e49a0204011292abb91
the-icarus <shorty2292@googlemail.com>
2021-03-01 17:31:34 +0100

    add custom pre- and postpack to add type definitions to final asset

---
+++ tooling/postpack.js
@@ -0,0 +1,4 @@
+const fs = require('fs-extra');
+
+fs.remove('es')
+  .then(() => fs.move('es_src', 'es'));
---
+++ tooling/prepack.js
@@ -0,0 +1,5 @@
+const fs = require('fs-extra');
+
+fs.move('es', 'es_src')
+  .then(() => fs.remove('es'))
+  .then(() => fs.move('dist/es', 'es'));
