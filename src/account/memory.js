b7257c93476853d7e01169d3fab9d8a626afc2c8
Denis Davidyuk <denis_davidyuk@hotmail.com>
2021-05-01 14:16:42 +0300

    refactor(account-memory): inline helpers

--- src/account/memory.js
+++ src/account/memory.js
@@ -23,40 +23,11 @@
  */
 
 import AccountBase from './base'
-import { sign as cryptoSign, isAddressValid, isValidKeypair } from '../utils/crypto'
+import { sign, isAddressValid, isValidKeypair } from '../utils/crypto'
 import { isHex } from '../utils/string'
 import { decode } from '../tx/builder/helpers'
 
 const secrets = new WeakMap()
-
-async function sign (data) {
-  if (this.isGa) throw new Error('You are trying to sign data using GA account without keypair')
-  return Promise.resolve(cryptoSign(data, secrets.get(this).secretKey))
-}
-
-async function address () {
-  return Promise.resolve(secrets.get(this).publicKey)
-}
-
-function setSecret (keyPair) {
-  secrets.set(this, {
-    secretKey: Buffer.isBuffer(keyPair.secretKey) ? keyPair.secretKey : Buffer.from(keyPair.secretKey, 'hex'),
-    publicKey: keyPair.publicKey
-  })
-}
-
-function validateKeyPair (keyPair) {
-  if (!keyPair || typeof keyPair !== 'object') throw new Error('KeyPair must be an object')
-  if (!keyPair.secretKey || !keyPair.publicKey) throw new Error('KeyPair must must have "secretKey", "publicKey" properties')
-  if (typeof keyPair.publicKey !== 'string' || keyPair.publicKey.indexOf('ak_') === -1) throw new Error('Public Key must be a base58c string with "ak_" prefix')
-  if (
-    !Buffer.isBuffer(keyPair.secretKey) &&
-    (typeof keyPair.secretKey === 'string' && !isHex(keyPair.secretKey))
-  ) throw new Error('Secret key must be hex string or Buffer')
-
-  const pubBuffer = Buffer.from(decode(keyPair.publicKey, 'ak'))
-  if (!isValidKeypair(Buffer.from(keyPair.secretKey, 'hex'), pubBuffer)) throw new Error('Invalid Key Pair')
-}
 
 /**
  * In-memory account stamp
@@ -75,12 +46,33 @@ export default AccountBase.compose({
     if (gaId) {
       if (!isAddressValid(gaId)) throw new Error('Invalid GA address')
       secrets.set(this, { publicKey: gaId })
-    } else {
-      validateKeyPair(keypair)
-
-      this.setSecret(keypair)
+      return
     }
+
+    if (!keypair || typeof keypair !== 'object') throw new Error('KeyPair must be an object')
+    if (!keypair.secretKey || !keypair.publicKey) throw new Error('KeyPair must must have "secretKey", "publicKey" properties')
+    if (typeof keypair.publicKey !== 'string' || keypair.publicKey.indexOf('ak_') === -1) throw new Error('Public Key must be a base58c string with "ak_" prefix')
+    if (
+      !Buffer.isBuffer(keypair.secretKey) &&
+      (typeof keypair.secretKey === 'string' && !isHex(keypair.secretKey))
+    ) throw new Error('Secret key must be hex string or Buffer')
+
+    const pubBuffer = Buffer.from(decode(keypair.publicKey, 'ak'))
+    if (!isValidKeypair(Buffer.from(keypair.secretKey, 'hex'), pubBuffer)) throw new Error('Invalid Key Pair')
+
+    secrets.set(this, {
+      secretKey: Buffer.isBuffer(keypair.secretKey) ? keypair.secretKey : Buffer.from(keypair.secretKey, 'hex'),
+      publicKey: keypair.publicKey
+    })
   },
   props: { isGa: false },
-  methods: { sign, address, setSecret }
+  methods: {
+    sign (data) {
+      if (this.isGa) throw new Error('You are trying to sign data using GA account without keypair')
+      return Promise.resolve(sign(data, secrets.get(this).secretKey))
+    },
+    address () {
+      return Promise.resolve(secrets.get(this).publicKey)
+    }
+  }
 })
