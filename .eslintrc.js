c89566eaf5f24dc5b448f43463384e3b6b2acc0b
Denis Davidyuk <denis_davidyuk@hotmail.com>
2021-11-14 17:05:42 +0300

    chore: generate configuration using `eslint --init`

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'standard'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module'
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  rules: {
+  }
+}
