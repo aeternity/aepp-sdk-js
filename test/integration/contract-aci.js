f963bdbf2f5eb9284c44b3191c4d61555694030d
Philipp Piwo <github@piwo.space>
2021-11-25 17:49:16 +0100

    fix(events): don't require function name for events decoding

--- test/integration/contract-aci.js
+++ test/integration/contract-aci.js
@@ -277,7 +277,7 @@ describe('Contract instance', function () {
       eventResult = await cInstance.methods.emitEvents()
       const { log } = await sdk.tx(eventResult.hash)
       decodedEventsWithoutACI = decodeEvents(log, events)
-      decodedEventsUsingACI = cInstance.decodeEvents('emitEvents', log)
+      decodedEventsUsingACI = cInstance.decodeEvents(log)
       decodedEventsUsingBuildInMethod = cInstance.methods.emitEvents.decodeEvents(log)
     })
 
