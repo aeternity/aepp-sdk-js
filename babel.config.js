9f2f22a97c84b00efda5f11373836eddfaa26f50
Denis Davidyuk <denis_davidyuk@hotmail.com>
2022-01-26 11:17:54 +0300

    refactor(wallet-web-extension): use vue-cli-plugin-browser-extension

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,8 @@ module.exports = {
   presets: [
     ['@babel/preset-env', {
       include: [
-        '@babel/plugin-proposal-nullish-coalescing-operator'
+        '@babel/plugin-proposal-nullish-coalescing-operator',
+        '@babel/plugin-proposal-logical-assignment-operators'
       ]
     }],
     '@babel/preset-typescript'
---
+++ examples/browser/wallet-web-extension/src/Popup.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h3>Wallet WebExtension</h3>
+    <img src="../public/icons/128.png" alt="Logo">
+  </div>
+</template>
+
+<style>
+html {
+  width: 400px;
+  height: 400px;
+}
+</style>
