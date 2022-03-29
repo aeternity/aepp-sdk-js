57778e029acb031a79405a5b2d37bbef91fe4ca9
Denis Davidyuk <denis_davidyuk@hotmail.com>
2022-03-29 13:44:01 +0300

    refactor: extract encoder module and convert it to TS

--- src/tx/builder/helpers.js
+++ src/tx/builder/helpers.js
@@ -1,6 +1,6 @@
 import BigNumber from 'bignumber.js'
-import bs58 from 'bs58'
-import { hash, salt, sha256hash } from '../../utils/crypto'
+import { hash, salt } from '../../utils/crypto'
+import { encode, decode } from '../../utils/encoder'
 import { toBytes } from '../../utils/bytes'
 import {
   ID_TAG_PREFIX,
@@ -13,17 +13,12 @@ import {
 } from './schema'
 import { ceil } from '../../utils/bignumber'
 import {
-  PrefixMismatchError,
-  DecodeError,
-  EncodeError,
-  PayloadLengthError,
   TagNotFoundError,
   PrefixNotFoundError,
   InvalidNameError,
   IllegalBidFeeError,
   NoDefaultAensPointerError,
-  ArgumentError,
-  InvalidChecksumError
+  ArgumentError
 } from '../../utils/errors'
 
 /**
@@ -34,6 +29,8 @@ import {
  */
 
 export const createSalt = salt
+
+export { encode, decode }
 
 /**
  * Build a contract public key
@@ -106,89 +103,6 @@ export function produceNameId (name) {
 export function commitmentHash (name, salt = createSalt()) {
   ensureNameValid(name)
   return encode(hash(Buffer.concat([Buffer.from(name.toLowerCase()), formatSalt(salt)])), 'cm')
-}
-
-// based on https://github.com/aeternity/protocol/blob/master/node/api/api_encoding.md
-const base64Types = ['ba', 'cb', 'or', 'ov', 'pi', 'ss', 'cs', 'ck', 'cv', 'st', 'tx']
-const base58Types = ['ak', 'bf', 'bs', 'bx', 'ch', 'cm', 'ct', 'kh', 'mh', 'nm', 'ok', 'oq', 'pp', 'sg', 'th']
-// TODO: add all types with a fixed length
-const typesLength = {
-  ak: 32,
-  ct: 32,
-  ok: 32
-}
-
-function ensureValidLength (data, type) {
-  if (!typesLength[type]) return
-  if (data.length === typesLength[type]) return
-  throw new PayloadLengthError(`Payload should be ${typesLength[type]} bytes, got ${data.length} instead`)
-}
-
-const getChecksum = payload => sha256hash(sha256hash(payload)).slice(0, 4)
-
-const addChecksum = (input) => {
-  const payload = Buffer.from(input)
-  return Buffer.concat([payload, getChecksum(payload)])
-}
-
-function getPayload (buffer) {
-  const payload = buffer.slice(0, -4)
-  if (!getChecksum(payload).equals(buffer.slice(-4))) throw new InvalidChecksumError()
-  return payload
-}
-
-const base64 = {
-  encode: buffer => addChecksum(buffer).toString('base64'),
-  decode: string => getPayload(Buffer.from(string, 'base64'))
-}
-
-const base58 = {
-  encode: buffer => bs58.encode(addChecksum(buffer)),
-  decode: string => getPayload(bs58.decode(string))
-}
-
-/**
- * Decode data using the default encoding/decoding algorithm
- * @function
- * @alias module:@aeternity/aepp-sdk/es/tx/builder/helpers
- * @param {string} data An Base58/64check encoded and prefixed string (ex tx_..., sg_..., ak_....)
- * @param {string} [requiredPrefix] Ensure that data have this prefix
- * @return {Buffer} Decoded data
- */
-export function decode (data, requiredPrefix) {
-  if (typeof data !== 'string') throw new DecodeError(`Encoded should be a string, got ${data} instead`)
-  const [prefix, encodedPayload, extra] = data.split('_')
-  if (!encodedPayload) throw new DecodeError(`Encoded string missing payload: ${data}`)
-  if (extra) throw new DecodeError(`Encoded string have extra parts: ${data}`)
-  if (requiredPrefix && requiredPrefix !== prefix) {
-    throw new PrefixMismatchError(prefix, requiredPrefix)
-  }
-  const decoder = (base64Types.includes(prefix) && base64.decode) ||
-    (base58Types.includes(prefix) && base58.decode)
-  if (!decoder) {
-    throw new DecodeError(`Encoded string have unknown type: ${prefix}`)
-  }
-  const payload = decoder(encodedPayload)
-  ensureValidLength(payload, prefix)
-  return payload
-}
-
-/**
- * Encode data using the default encoding/decoding algorithm
- * @function
- * @alias module:@aeternity/aepp-sdk/es/tx/builder/helpers
- * @param {Buffer|String} data  An decoded data
- * @param {string} type Prefix of Transaction
- * @return {String} Encoded string Base58check or Base64check data
- */
-export function encode (data, type) {
-  const encoder = (base64Types.includes(type) && base64.encode) ||
-    (base58Types.includes(type) && base58.encode)
-  if (!encoder) {
-    throw new EncodeError(`Unknown type: ${type}`)
-  }
-  ensureValidLength(data, type)
-  return `${type}_${encoder(data)}`
 }
 
 /**
