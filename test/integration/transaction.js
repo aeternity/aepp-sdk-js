4d4fedaeb00d324ac3a770a68080f27f3b6a7026
Denis Davidyuk <denis_davidyuk@hotmail.com>
2022-03-30 14:30:58 +0300

    test(transaction): hardcode debug endpoint responses

--- test/integration/transaction.js
+++ test/integration/transaction.js
@@ -16,11 +16,9 @@
  */
 
 import { describe, it, before } from 'mocha'
-import { generateKeyPair, salt } from '../../src/utils/crypto'
-import { getSdk } from './index'
-import { commitmentHash, oracleQueryId, encode } from '../../src/tx/builder/helpers'
+import { BaseAe, spendPromise } from './index'
+import { commitmentHash, oracleQueryId } from '../../src/tx/builder/helpers'
 import { GAS_MAX, MIN_GAS_PRICE } from '../../src/tx/builder/schema'
-import { MemoryAccount } from '../../src'
 import { AE_AMOUNT_FORMATS } from '../../src/utils/amount-formatter'
 import { unpackTx } from '../../src/tx/builder'
 
@@ -53,8 +51,8 @@ contract Identity =
 let contractId
 const deposit = 0
 
-let _salt
-let commitmentId
+const nameSalt = 4204563566073083
+const commitmentId = commitmentHash(name, nameSalt)
 
 describe('Native Transaction', function () {
   let aeSdkNative
@@ -63,15 +61,10 @@ describe('Native Transaction', function () {
   let queryId
 
   before(async () => {
-    const keyPair = generateKeyPair()
-    aeSdk = await getSdk({ nativeMode: false })
-    aeSdkNative = await getSdk()
-    await aeSdk.spend('16774200000000000000', keyPair.publicKey)
-    await aeSdk.addAccount(MemoryAccount({ keypair: keyPair }), { select: true })
-    await aeSdkNative.addAccount(MemoryAccount({ keypair: keyPair }), { select: true })
+    aeSdk = await BaseAe({ nativeMode: false })
+    aeSdkNative = await BaseAe()
+    await spendPromise
     oracleId = `ok_${(await aeSdk.address()).slice(3)}`
-    _salt = salt()
-    commitmentId = await commitmentHash(name, _salt)
   })
 
   it('Build tx using denomination amount', async () => {
@@ -88,9 +81,8 @@ describe('Native Transaction', function () {
 
   it('native build of spend tx', async () => {
     const aeAmount = 2
-    const aettosAmount = 2e18
     const params = { senderId, recipientId, nonce, payload: 'test' }
-    const txFromAPI = await aeSdk.spendTx({ ...params, amount: aettosAmount })
+    const txFromAPI = 'tx_+F0MAaEB4TK48d23oE5jt/qWR5pUu8UlpTGn8bwM5JISGQMGf7ChAeEyuPHdt6BOY7f6lkeaVLvFJaUxp/G8DOSSEhkDBn+wiBvBbWdOyAAAhg9e1n8oAAABhHRlc3QLK3OW'
     const nativeTx = await aeSdkNative.spendTx(
       { ...params, amount: aeAmount, denomination: AE_AMOUNT_FORMATS.AE }
     )
@@ -99,14 +91,14 @@ describe('Native Transaction', function () {
 
   it('native build of name pre-claim tx', async () => {
     const params = { accountId: senderId, nonce, commitmentId }
-    const txFromAPI = await aeSdk.namePreclaimTx(params)
+    const txFromAPI = 'tx_+E8hAaEB4TK48d23oE5jt/qWR5pUu8UlpTGn8bwM5JISGQMGf7ABoQOvDVCf43V7alNbsUvTarXaCf7rjtWX36YLS4+JTa4jn4YPHaUyOAAAxRZ6Sg=='
     const nativeTx = await aeSdkNative.namePreclaimTx(params)
     txFromAPI.should.be.equal(nativeTx)
   })
 
   it('native build of claim tx', async () => {
-    const params = { accountId: senderId, nonce, name, nameSalt: _salt, nameFee }
-    const txFromAPI = await aeSdk.nameClaimTx(params)
+    const params = { accountId: senderId, nonce, name, nameSalt, nameFee }
+    const txFromAPI = 'tx_+FEgAqEB4TK48d23oE5jt/qWR5pUu8UlpTGn8bwM5JISGQMGf7ABkXRlc3QxMjN0ZXN0LmNoYWluhw7wBz3KlPuJNjXJrcXeoAAAhg8m9WHIAABl9JBX'
     const nativeTx = await aeSdkNative.nameClaimTx(params)
     txFromAPI.should.be.equal(nativeTx)
   })
@@ -114,20 +106,20 @@ describe('Native Transaction', function () {
   it('native build of update tx', async () => {
     const params = { accountId: senderId, nonce, nameId, nameTtl, pointers, clientTtl }
     const nativeTx = await aeSdkNative.nameUpdateTx(params)
-    const txFromAPI = await aeSdk.nameUpdateTx(params)
+    const txFromAPI = 'tx_+IQiAaEB4TK48d23oE5jt/qWR5pUu8UlpTGn8bwM5JISGQMGf7ABoQL1zlEz+3+D5h4MF9POub3zp5zJ2fj6VUWGMNOhCyMYPAHy8Y5hY2NvdW50X3B1YmtleaEB4TK48d23oE5jt/qWR5pUu8UlpTGn8bwM5JISGQMGf7ABhhAUch6gAADR52s+'
     txFromAPI.should.be.equal(nativeTx)
   })
 
   it('native build of revoke tx', async () => {
     const params = { accountId: senderId, nonce, nameId }
-    const txFromAPI = await aeSdk.nameRevokeTx(params)
+    const txFromAPI = 'tx_+E8jAaEB4TK48d23oE5jt/qWR5pUu8UlpTGn8bwM5JISGQMGf7ABoQL1zlEz+3+D5h4MF9POub3zp5zJ2fj6VUWGMNOhCyMYPIYPHaUyOAAA94BVgw=='
     const nativeTx = await aeSdkNative.nameRevokeTx(params)
     txFromAPI.should.be.equal(nativeTx)
   })
 
   it('native build of transfer tx', async () => {
     const params = { accountId: senderId, nonce, nameId, recipientId }
-    const txFromAPI = await aeSdk.nameTransferTx(params)
+    const txFromAPI = 'tx_+HEkAaEB4TK48d23oE5jt/qWR5pUu8UlpTGn8bwM5JISGQMGf7ABoQL1zlEz+3+D5h4MF9POub3zp5zJ2fj6VUWGMNOhCyMYPKEB4TK48d23oE5jt/qWR5pUu8UlpTGn8bwM5JISGQMGf7CGD7v4WsgAAL1d+NM='
     const nativeTx = await aeSdkNative.nameTransferTx(params)
     txFromAPI.should.be.equal(nativeTx)
   })
@@ -137,6 +129,7 @@ describe('Native Transaction', function () {
     contract = await aeSdk.getContractInstance({ source: contractSource })
     await contract.compile()
     const params = {
+      nonce,
       ownerId: await aeSdk.address(),
       code: contract.bytecode,
       deposit,
@@ -145,13 +138,14 @@ describe('Native Transaction', function () {
       gasPrice: MIN_GAS_PRICE,
       callData: contract.calldata.encode('Identity', 'init', [])
     }
-    const txFromAPI = await aeSdk.contractCreateTx(params)
+    const txFromAPI = {
+      contractId: 'ct_TCQVoset7Y4qEyV5tgEAJAqa2Foz8J1EXqoGpq3fB6dWH5roe',
+      tx: 'tx_+LAqAaEB1c8IQA6YgiLybrSwLI+JB3RXRnIRpubZVe23B0nGozsBuGr4aEYDoKEijZbj/w2AeiWwAbldusME5pm3ZgPuomnZ3TbUbYgrwLg7nv5E1kQfADcANwAaDoI/AQM//oB4IJIANwEHBwEBAJgvAhFE1kQfEWluaXQRgHggkhlnZXRBcmeCLwCFNi4xLjAAgwcAA4ZHcyzkwAAAAACDGBf4hDuaygCHKxFE1kQfP+Jcll0='
+    }
     const nativeTx = await aeSdkNative.contractCreateTx(params)
 
     txFromAPI.tx.should.be.equal(nativeTx.tx)
     txFromAPI.contractId.should.be.equal(nativeTx.contractId)
-    // deploy contract
-    await aeSdk.send(nativeTx.tx)
     contractId = txFromAPI.contractId
   })
 
@@ -160,6 +154,7 @@ describe('Native Transaction', function () {
     const owner = await aeSdk.address()
 
     const params = {
+      nonce,
       callerId: owner,
       contractId,
       amount,
@@ -167,75 +162,52 @@ describe('Native Transaction', function () {
       gasPrice: MIN_GAS_PRICE,
       callData
     }
-    const txFromAPI = await aeSdk.contractCallTx(params)
+    const txFromAPI = 'tx_+GMrAaEB1c8IQA6YgiLybrSwLI+JB3RXRnIRpubZVe23B0nGozsBoQU7e5ChtHAGM1Nh0MVEV74SbrYb1b5FQ3WBd7OBpwALyQOGpYvVcSgAAACDGBf4hDuaygCIKxGAeCCSGwQL3c3m'
     const nativeTx = await aeSdkNative.contractCallTx(params)
     txFromAPI.should.be.equal(nativeTx)
-
-    const { hash } = await aeSdk.send(nativeTx)
-    const { callInfo: { returnType } } = await aeSdk.api.getTransactionInfoByHash(hash)
-
-    returnType.should.be.equal('ok')
   })
 
   it('native build of oracle create tx', async () => {
     const accountId = await aeSdk.address()
-    const params = { accountId, queryFormat, responseFormat, queryFee, oracleTtl }
+    const params = { nonce, accountId, queryFormat, responseFormat, queryFee, oracleTtl }
 
-    const txFromAPI = await aeSdk.oracleRegisterTx(params)
+    const txFromAPI = 'tx_+FAWAaEB1c8IQA6YgiLybrSwLI+JB3RXRnIRpubZVe23B0nGozsBjXsnY2l0eSc6IHN0cn2Meyd0bXAnOiBudW19gnUwAIIB9IYPN7jqmAAAAGsRIcw='
     const nativeTx = await aeSdkNative.oracleRegisterTx(params)
 
     txFromAPI.should.be.equal(nativeTx)
-    await aeSdkNative.send(nativeTx)
-
-    const oId = (await aeSdk.api.getOracleByPubkey(oracleId)).id
-    oId.should.be.equal(oracleId)
   })
 
   it('native build of oracle extends tx', async () => {
     const callerId = await aeSdk.address()
-    const params = { oracleId, callerId, oracleTtl }
-    const orTtl = (await aeSdk.api.getOracleByPubkey(oracleId)).ttl
+    const params = { nonce, oracleId, callerId, oracleTtl }
 
-    const txFromAPI = await aeSdk.oracleExtendTx(params)
+    const txFromAPI = 'tx_8RkBoQTVzwhADpiCIvJutLAsj4kHdFdGchGm5tlV7bcHScajOwEAggH0hg6itfGYAADwE/X7'
     const nativeTx = await aeSdkNative.oracleExtendTx(params)
 
     txFromAPI.should.be.equal(nativeTx)
-
-    await aeSdk.send(nativeTx)
-    const orNewTtl = (await aeSdk.api.getOracleByPubkey(oracleId)).ttl
-    orNewTtl.should.be.equal(orTtl + oracleTtl.value)
   })
 
   it('native build of oracle post query tx', async () => {
     const senderId = await aeSdk.address()
 
-    const params = { oracleId, responseTtl, query, queryTtl, queryFee, senderId }
+    const params = { nonce, oracleId, responseTtl, query, queryTtl, queryFee, senderId }
 
-    const txFromAPI = await aeSdk.oraclePostQueryTx(params)
+    const txFromAPI = 'tx_+GkXAaEB1c8IQA6YgiLybrSwLI+JB3RXRnIRpubZVe23B0nGozsBoQTVzwhADpiCIvJutLAsj4kHdFdGchGm5tlV7bcHScajO5J7J2NpdHknOiAnQmVybGluJ32CdTAAZABkhg+bJBmGAAAtn7nr'
     const nativeTx = await aeSdkNative.oraclePostQueryTx(params)
     queryId = oracleQueryId(senderId, unpackTx(txFromAPI).tx.nonce, oracleId)
 
     txFromAPI.should.be.equal(nativeTx)
-
-    await aeSdk.send(nativeTx)
-
-    const oracleQuery = (await aeSdk.api.getOracleQueryByPubkeyAndQueryId(oracleId, queryId))
-    oracleQuery.id.should.be.equal(queryId)
   })
 
   it('native build of oracle respond query tx', async () => {
     const callerId = await aeSdk.address()
-    const params = { oracleId, callerId, responseTtl, queryId, response: queryResponse }
+    const params = { nonce, oracleId, callerId, responseTtl, queryId, response: queryResponse }
 
-    const txFromAPI = await aeSdk.oracleRespondTx(params)
+    const txFromAPI = 'tx_+F0YAaEE1c8IQA6YgiLybrSwLI+JB3RXRnIRpubZVe23B0nGozsBoClgM30zCmbxGvUfzRbIZXGzOT8KCzYAUMRdnxbBX2Q9jHsndG1wJzogMTAxfQBkhg9jQvwmAADfRUs7'
     const nativeTx = await aeSdkNative.oracleRespondTx(params)
     txFromAPI.should.be.equal(nativeTx)
-
-    await aeSdk.send(nativeTx)
-
-    const orQuery = (await aeSdk.api.getOracleQueryByPubkeyAndQueryId(oracleId, queryId))
-    orQuery.response.should.be.equal(encode(queryResponse, 'or'))
   })
+
   it('Get next account nonce', async () => {
     const accountId = await aeSdk.address()
     const { nonce: accountNonce } = await aeSdk.api.getAccountByPubkey(accountId)
@@ -245,6 +217,7 @@ describe('Native Transaction', function () {
     const nonceCustom = await aeSdk.getAccountNonce(await aeSdk.address(), 1)
     nonceCustom.should.be.equal(1)
   })
+
   it('Destroy instance finishes without error', () => {
     aeSdk.destroyInstance()
   })
