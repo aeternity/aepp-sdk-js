unicorndomaingr/aepp-sdk-js-ts

refactor(node)!: drop `internalUrl`
BREAKING CHANGE: removed `internalUrl`
`Node` doesn't accepts and stores `internalUrl`, also internal endpoints are not available anymore.
If necessary, create a wrapper of internal API separately (using `genSwaggerClient`).

BREAKING CHANGE: removed `mempool` method
Create a wrapper of internal API by `genSwaggerClient` and use `getPendingTransactions` method
instead.

Reverts: 690db5bd2919958edcadca79fd75b5ad96b77c62

test/integration/rpc.js
@@ -1,6 +1,6 @@
 /*
  * ISC License (ISC)
- * Copyright (c) 2021 aeternity developers
+ * Copyright (c) 2022 aeternity developers
  *
  *  Permission to use, copy, modify, and/or distribute this software for any
  *  purpose with or without fee is hereby granted, provided that the above
@@ -24,7 +24,7 @@ import BrowserWindowMessageConnection from '../../src/utils/aepp-wallet-communic
 import { getBrowserAPI, getHandler } from '../../src/utils/aepp-wallet-communication/helpers'
 import { METHODS, RPC_STATUS } from '../../src/utils/aepp-wallet-communication/schema'
 import { generateKeyPair, verify, hash } from '../../src/utils/crypto'
-import { compilerUrl, genesisAccount, internalUrl, networkId, publicKey, url, ignoreVersion } from './'
+import { compilerUrl, genesisAccount, networkId, publicKey, url, ignoreVersion } from './'
 import {
   NoBrowserFoundError,
   NoWalletConnectedError,
@@ -42,7 +42,7 @@ describe('Aepp<->Wallet', function () {
   let connectionFromAeppToWallet
 
   before(async function () {
-    node = await Node({ url, internalUrl, ignoreVersion })
+    node = await Node({ url, ignoreVersion })
     connections = getConnections()
     connectionFromWalletToAepp = BrowserWindowMessageConnection({
       connectionInfo: { id: 'from_wallet_to_aepp' },

aeternity/aepp-sdk-js

test/integration/rpc.js
@@ -1,6 +1,6 @@
 /*
  * ISC License (ISC)
- * Copyright (c) 2021 aeternity developers
+ * Copyright (c) 2022 aeternity developers
  *
  *  Permission to use, copy, modify, and/or distribute this software for any
  *  purpose with or without fee is hereby granted, provided that the above
@@ -24,7 +24,7 @@ import BrowserWindowMessageConnection from '../../src/utils/aepp-wallet-communic
 import { getBrowserAPI, getHandler } from '../../src/utils/aepp-wallet-communication/helpers'
 import { METHODS, RPC_STATUS } from '../../src/utils/aepp-wallet-communication/schema'
 import { generateKeyPair, verify, hash } from '../../src/utils/crypto'
-import { compilerUrl, account, internalUrl, networkId, url, ignoreVersion, spendPromise } from './'
+import { compilerUrl, account, networkId, url, ignoreVersion, spendPromise } from './'
 import {
   NoBrowserFoundError,
   NoWalletConnectedError,
@@ -42,7 +42,7 @@ describe('Aepp<->Wallet', function () {
   let connectionFromAeppToWallet
 
   before(async function () {
-    node = await Node({ url, internalUrl, ignoreVersion })
+    node = await Node({ url, ignoreVersion })
     connections = getConnections()
     connectionFromWalletToAepp = BrowserWindowMessageConnection({
       connectionInfo: { id: 'from_wallet_to_aepp' },