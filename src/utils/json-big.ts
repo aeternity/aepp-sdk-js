76267c1241dad920f31cd94aabd3175c92f95a99
martinkaintas <martinkaintas@gmail.com>
2022-04-25 14:03:26 +0300

    refactor(utils): migrate json-big.js to TS

--- src/utils/json-big.ts
+++ src/utils/json-big.ts
@@ -4,15 +4,15 @@ import { mapObject } from './other'
 
 const jsonBig = JsonBig({ storeAsString: true })
 
-const convertValuesToBigNumbers = value => {
+const convertValuesToBigNumbers = (value: any): any => {
   if (typeof value === 'object' && value !== null && value.constructor === Object) {
     return mapObject(value, ([key, value]) => [key, convertValuesToBigNumbers(value)])
   }
   if (Array.isArray(value)) {
     return value.map(item => convertValuesToBigNumbers(item))
   }
-  if (typeof value === 'string' && BigNumber(value).toString(10) === value) {
-    const bn = BigNumber(value)
+  if (typeof value === 'string' && new BigNumber(value).toString(10) === value) {
+    const bn = new BigNumber(value)
     bn.toJSON = () => bn.toString(10)
     return bn
   }
@@ -20,6 +20,7 @@ const convertValuesToBigNumbers = value => {
 }
 
 export default {
-  stringify: (object, ...args) => jsonBig.stringify(convertValuesToBigNumbers(object), ...args),
+  stringify: (...args: Parameters<typeof JsonBig['stringify']>): string =>
+    jsonBig.stringify(convertValuesToBigNumbers(args[0]), ...args.slice(1)),
   parse: jsonBig.parse
 }
