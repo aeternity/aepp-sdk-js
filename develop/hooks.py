894ef562e76ed59fd4b1bb585d360c2ce3ac76be
GitHub Action <github-action@users.noreply.github.com>
2022-05-23 20:30:19 +0000

    Deployed 8b0614c4 to develop with MkDocs 1.2.4 and mike 1.0.1

--- develop/hooks.py
+++ develop/hooks.py
@@ -3,6 +3,6 @@ import subprocess
 
 def pre_build(**kwargs):
   if not os.path.exists('node_modules'):
-    subprocess.run(['npm', 'install'], check=True)
+    subprocess.run(['npm', 'install', '--ignore-scripts'], check=True)
   subprocess.run(['npm', 'run', 'docs:examples'], check=True)
   # subprocess.run(['npm', 'run', 'docs:api'], check=True)
