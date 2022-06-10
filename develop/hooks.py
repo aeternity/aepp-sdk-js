792b6f28484d0a81591377640297bbe96379d3d3
Denis Davidyuk <denis_davidyuk@hotmail.com>
2022-06-10 16:02:01 +0400

    Cleanup develop docs

--- develop/hooks.py
+++
@@ -1,8 +0,0 @@
-import os.path
-import subprocess
-
-def pre_build(**kwargs):
-  if not os.path.exists('node_modules'):
-    subprocess.run(['npm', 'install', '--ignore-scripts'], check=True)
-  subprocess.run(['npm', 'run', 'docs:examples'], check=True)
-  # subprocess.run(['npm', 'run', 'docs:api'], check=True)
