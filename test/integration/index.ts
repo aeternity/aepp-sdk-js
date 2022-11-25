4046262614cd29c8dffd1f4bf443d7cd00e4b5fc
Denis Davidyuk <denis_davidyuk@hotmail.com>
2022-11-25 16:27:50 +0400

    test(compiler): extract tests

--- test/integration/index.ts
+++ test/integration/index.ts
@@ -19,7 +19,7 @@ import { AeSdk, MemoryAccount, Node } from '../../src';
 import '..';
 
 export const url = process.env.TEST_URL ?? 'http://localhost:3013';
-const compilerUrl = process.env.COMPILER_URL ?? 'http://localhost:3080';
+export const compilerUrl = process.env.COMPILER_URL ?? 'http://localhost:3080';
 const secretKey = process.env.SECRET_KEY ?? 'bf66e1c256931870908a649572ed0257876bb84e3cdf71efb12f56c7335fad54d5cf08400e988222f26eb4b02c8f89077457467211a6e6d955edb70749c6a33b';
 export const networkId = process.env.TEST_NETWORK_ID ?? 'ae_devnet';
 export const ignoreVersion = process.env.IGNORE_VERSION === 'true';
