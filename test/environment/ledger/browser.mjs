3568c43db98d540b7326a2a97d37a03788ee337c
Denis Davidyuk <denis_davidyuk@hotmail.com>
2023-03-16 13:17:37 +0400

    test(account): replace document.write with innerHTML to fix error in Ledger
    > Failed to execute 'write' on 'Document': It isn't possible to write into a document from an
    asynchronously-loaded external script unless it is explicitly opened.

--- test/environment/ledger/browser.mjs
+++ test/environment/ledger/browser.mjs
@@ -2,10 +2,10 @@ import TransportWebUSB from '@ledgerhq/hw-transport-webusb';
 // eslint-disable-next-line import/extensions
 import run from './main.mjs';
 
-document.write(`
+document.body.innerHTML = `
   Open developer console
   <button id="run">Run</button>
-`);
+`;
 document.getElementById('run').addEventListener('click', async () => {
   const transport = await TransportWebUSB.create();
   await run(transport);
