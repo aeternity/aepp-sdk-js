1d9b6d63d288cb65b96e79671219de20ca484443
Denis Davidyuk <denis_davidyuk@hotmail.com>
2023-03-16 13:15:48 +0400

    test(account): fix Ledger init in browser
    > Failed to execute 'requestDevice' on 'USB': Must be handling a user gesture to show a permission
    request.

--- test/environment/ledger/browser.mjs
+++ test/environment/ledger/browser.mjs
@@ -2,9 +2,11 @@ import TransportWebUSB from '@ledgerhq/hw-transport-webusb';
 // eslint-disable-next-line import/extensions
 import run from './main.mjs';
 
-const transport = await TransportWebUSB.create();
 document.write(`
   Open developer console
   <button id="run">Run</button>
 `);
-document.getElementById('run').addEventListener('click', run.bind(null, transport));
+document.getElementById('run').addEventListener('click', async () => {
+  const transport = await TransportWebUSB.create();
+  await run(transport);
+});
