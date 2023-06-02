7c46ed784b7e263db74e10a6ed1766c24a6b643f
Denis Davidyuk <denis_davidyuk@hotmail.com>
2023-06-02 11:07:53 +0600

    ci(docs): generate report for example aepps

--- docs/build-assets.sh
+++ docs/build-assets.sh
@@ -12,20 +12,22 @@ mkdir -p docs/examples/browser
 echo Build example aepp
 cd ./examples/browser/aepp
 npm i
-VUE_APP_WALLET_URL=../wallet-iframe/ PUBLIC_PATH=./ npm run build
+VUE_APP_WALLET_URL=../wallet-iframe/ PUBLIC_PATH=./ npm run build -- --report
 mv -f dist/ ../../../docs/examples/browser/aepp
 
 echo Build example wallet-iframe
 cd ../wallet-iframe
 npm i
-VUE_APP_AEPP_URL=../aepp/ PUBLIC_PATH=./ npm run build
+VUE_APP_AEPP_URL=../aepp/ PUBLIC_PATH=./ npm run build -- --report
 mv -f dist/ ../../../docs/examples/browser/wallet-iframe
 
 echo Build example wallet-web-extension
 cd ../wallet-web-extension
 npm i
-NODE_OPTIONS=--openssl-legacy-provider npm run build
-mv artifacts/wallet-web-extension-v0.1.0-production.zip ../../../docs/examples/browser/wallet-web-extension.zip
+NODE_OPTIONS=--openssl-legacy-provider npm run build -- --report
+mkdir ../../../docs/examples/browser/wallet-web-extension/
+mv artifacts/wallet-web-extension-v0.1.0-production.zip ../../../docs/examples/browser/wallet-web-extension/packed.zip
+mv dist/report.html ../../../docs/examples/browser/wallet-web-extension/report.html
 
 cd ../../..
 perl -i -pe 's/"rem-prepare"/"prepare"/g' package.json
