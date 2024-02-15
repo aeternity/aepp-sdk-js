a21bc1f85f6c51f940f4ca5d26b17bcc7b4d31e6
Denis Davidyuk <denis_davidyuk@hotmail.com>
2024-02-15 17:09:58 +0700

    test: adjust testnet timeouts

--- .mocharc.js
+++ .mocharc.js
@@ -2,7 +2,7 @@ module.exports = {
   require: 'tooling/babel-register.js',
   recursive: true,
   extension: '.js,.ts',
-  timeout: process.env.NETWORK ? '400s' : '40s',
+  timeout: process.env.NETWORK ? '500s' : '40s',
   ignore: 'test/environment/**',
   exit: true // TODO: fix in state channel tests
 }
