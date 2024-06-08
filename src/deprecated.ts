c521597e4ee4243266011d5e23e0fb75a126a404
Denis Davidyuk <denis_davidyuk@hotmail.com>
2024-06-08 17:11:45 +1000

    refactor(contract)!: remove `encodeFateValue`, `decodeFateValue`
    BREAKING CHANGE: `encodeFateValue`, `decodeFateValue` not exported anymore
    Use ContractByteArrayEncoder:encodeWithType, decodeWithType from `@aeternity/aepp-calldata`.

--- src/deprecated.ts
+++ src/deprecated.ts
@@ -1,6 +1,3 @@
-import { TypeResolver, ContractByteArrayEncoder } from '@aeternity/aepp-calldata';
-import { AciValue } from './utils/typed-data';
-import { Encoded } from './utils/encoder';
 import { RpcError } from './aepp-wallet-communication/schema';
 
 /**
@@ -23,19 +20,3 @@ export class RpcBroadcastError extends RpcError {
  * @deprecated use isAuctionName instead
  */
 export const NAME_BID_MAX_LENGTH = 12; // # this is the max length for a domain to be part of a bid
-
-/**
- * @deprecated use ContractByteArrayEncoder:encodeWithType from \@aeternity/aepp-calldata
- */
-export function encodeFateValue(value: unknown, aci: AciValue): Encoded.ContractBytearray {
-  const valueType = new TypeResolver().resolveType(aci, {});
-  return new ContractByteArrayEncoder().encodeWithType(value, valueType);
-}
-
-/**
- * @deprecated use ContractByteArrayEncoder:decodeWithType from \@aeternity/aepp-calldata
- */
-export function decodeFateValue(value: Encoded.ContractBytearray, aci: AciValue): any {
-  const valueType = new TypeResolver().resolveType(aci, {});
-  return new ContractByteArrayEncoder().decodeWithType(value, valueType);
-}
--- src/index-browser.ts
+++ src/index-browser.ts
@@ -112,6 +112,4 @@ export {
   TagNotFoundError, TxNotInChainError, AlreadyConnectedError, NoWalletConnectedError,
   RpcConnectionError,
 } from './utils/errors';
-export {
-  RpcBroadcastError, NAME_BID_MAX_LENGTH, encodeFateValue, decodeFateValue,
-} from './deprecated';
+export { RpcBroadcastError, NAME_BID_MAX_LENGTH } from './deprecated';
