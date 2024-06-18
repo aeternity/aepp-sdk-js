be9963a9ea109c2d1b631cc52d5ea7365cea5559
Denis Davidyuk <denis_davidyuk@hotmail.com>
2024-06-18 14:38:12 +1000

    docs: fix dry-run links in mkdocs

--- examples/node/dry-run-using-debug-endpoint.mjs
+++ examples/node/dry-run-using-debug-endpoint.mjs
@@ -2,25 +2,25 @@
 
 /*
 # Dry-run using debug endpoint
-By default, sdk uses [`protectedDryRunTxs`] endpoint limited to 6e6 gas by default.
+By default, sdk uses [protectedDryRunTxs] endpoint limited to 6e6 gas by default.
 It means if you try to dry-run a very big transaction (including intensive computations, or
 returning much data) it may be rejected with `Invocation failed: "Out of gas"` error.
 
 Ways to overcome this:
 - split transactions into multiple smaller transactions;
-- increase a gas limit for protected dry-runs by setting [`http.external.gas_limit`] in aenode
+- increase a gas limit for protected dry-runs by setting [http.external.gas_limit] in aenode
 configuration;
-- use [`dryRunTxs`] debug endpoint with the same api as a protected one.
+- use [dryRunTxs] debug endpoint with the same api as a protected one.
 
 The last options would work only for self-hosted nodes or testnet.aeternity.io which have debug
-endpoints exposed via [`http.endpoints.dry-run`]. Also, sdk doesn't know anything about debug
+endpoints exposed via [http.endpoints.dry-run]. Also, sdk doesn't know anything about debug
 endpoints. This script shows how to use a debug dry-run in sdk.
 
-[`protectedDryRunTxs`]: https://api-docs.aeternity.io/#/external/ProtectedDryRunTxs
+[protectedDryRunTxs]: https://api-docs.aeternity.io/#/external/ProtectedDryRunTxs
 <!-- TODO: move accurate links after fixing https://github.com/aeternity/aeternity/issues/4369 -->
-[`http.external.gas_limit`]: https://docs.aeternity.io/en/stable/configuration/
-[`http.endpoints.dry-run`]: https://docs.aeternity.io/en/stable/configuration/
-[`dryRunTxs`]: https://api-docs.aeternity.io/#/internal/DryRunTxs
+[http.external.gas_limit]: https://docs.aeternity.io/en/stable/configuration/
+[http.endpoints.dry-run]: https://docs.aeternity.io/en/stable/configuration/
+[dryRunTxs]: https://api-docs.aeternity.io/#/internal/DryRunTxs
 */
 
 import {
