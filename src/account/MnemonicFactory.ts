bd80a56ec49c8ae328e51d82ae843262ffd05a0f
Denis Davidyuk <denis_davidyuk@hotmail.com>
2024-07-03 18:15:24 +1000

    refactor(account): support wallet key in AccountMnemonicFactory

--- src/account/MnemonicFactory.ts
+++ src/account/MnemonicFactory.ts
@@ -1,22 +1,90 @@
 import { mnemonicToSeed } from '@scure/bip39';
+import { full as hmac } from 'tweetnacl-auth';
 import AccountBaseFactory from './BaseFactory';
 import AccountMemory from './Memory';
-import { generateSaveHDWalletFromSeed, getSaveHDWalletAccounts } from '../utils/hd-wallet';
-import { Encoding, encode } from '../utils/encoder';
+import {
+  encode, Encoding, Encoded, decode,
+} from '../utils/encoder';
+import { concatBuffers } from '../utils/other';
 import { InternalError } from '../utils/errors';
+
+const ED25519_CURVE = Buffer.from('ed25519 seed');
+const HARDENED_OFFSET = 0x80000000;
+
+interface KeyTreeNode {
+  secretKey: Uint8Array;
+  chainCode: Uint8Array;
+}
+
+function deriveKey(message: Uint8Array, key: Uint8Array): KeyTreeNode {
+  const I = hmac(message, key);
+  const IL = I.slice(0, 32);
+  const IR = I.slice(32);
+  return {
+    secretKey: IL,
+    chainCode: IR,
+  };
+}
+
+function derivePathFromKey(key: KeyTreeNode, segments: number[]): KeyTreeNode {
+  return segments.reduce(
+    ({ secretKey, chainCode }, segment) => {
+      const indexBuffer = Buffer.allocUnsafe(4);
+      indexBuffer.writeUInt32BE(segment + HARDENED_OFFSET, 0);
+      const data = concatBuffers([Buffer.alloc(1, 0), secretKey, indexBuffer]);
+      return deriveKey(data, chainCode);
+    },
+    key,
+  );
+}
+
+interface Wallet {
+  secretKey: Encoded.Bytearray;
+  chainCode: Encoded.Bytearray;
+}
 
 /**
  * A factory class that generates instances of AccountMemory based on provided mnemonic phrase.
  */
 export default class AccountMnemonicFactory extends AccountBaseFactory {
-  readonly #mnemonic: string;
+  readonly #mnemonic: string | undefined;
+
+  #wallet: Wallet | undefined;
 
   /**
-   * @param mnemonic - BIP39-compatible mnemonic phrase
+   * @param mnemonicOrWallet - BIP39-compatible mnemonic phrase or a wallet derived from mnemonic
    */
-  constructor(mnemonic: string) {
+  constructor(mnemonicOrWallet: string | Wallet) {
     super();
-    this.#mnemonic = mnemonic;
+    if (typeof mnemonicOrWallet === 'string') this.#mnemonic = mnemonicOrWallet;
+    else this.#wallet = mnemonicOrWallet;
+  }
+
+  /**
+   * Get a wallet to initialize AccountMnemonicFactory instead mnemonic phrase.
+   * In comparison with mnemonic, the wallet can be used to derive aeternity accounts only.
+   */
+  async getWallet(): Promise<Wallet> {
+    if (this.#wallet != null) return this.#wallet;
+    if (this.#mnemonic == null) throw new InternalError('AccountMnemonicFactory should be initialized with mnemonic or wallet');
+    const seed = await mnemonicToSeed(this.#mnemonic);
+    const masterKey = deriveKey(seed, ED25519_CURVE);
+    const walletKey = derivePathFromKey(masterKey, [44, 457]);
+    this.#wallet = {
+      secretKey: encode(walletKey.secretKey, Encoding.Bytearray),
+      chainCode: encode(walletKey.chainCode, Encoding.Bytearray),
+    };
+    return this.#wallet;
+  }
+
+  async #getAccountSecretKey(accountIndex: number): Promise<Encoded.AccountSecretKey> {
+    const wallet = await this.getWallet();
+    const walletKey = {
+      secretKey: decode(wallet.secretKey),
+      chainCode: decode(wallet.chainCode),
+    };
+    const raw = derivePathFromKey(walletKey, [accountIndex, 0, 0]).secretKey;
+    return encode(raw, Encoding.AccountSecretKey);
   }
 
   /**
@@ -24,15 +92,6 @@ export default class AccountMnemonicFactory extends AccountBaseFactory {
    * @param accountIndex - Index of account
    */
   async initialize(accountIndex: number): Promise<AccountMemory> {
-    // TODO: improve performance, don't use AES
-    const seed = await mnemonicToSeed(this.#mnemonic);
-    const wallet = generateSaveHDWalletFromSeed(seed, '');
-    const account = getSaveHDWalletAccounts(wallet, '', accountIndex + 1).at(-1);
-    if (account == null) throw new InternalError('Account can\'t be empty');
-    const secretKey = encode(
-      Buffer.from(account.secretKey, 'hex').subarray(0, 32),
-      Encoding.AccountSecretKey,
-    );
-    return new AccountMemory(secretKey);
+    return new AccountMemory(await this.#getAccountSecretKey(accountIndex));
   }
 }
--- test/unit/mnemonic.ts
+++ test/unit/mnemonic.ts
@@ -6,11 +6,27 @@ import {
 
 const mnemonic = 'eye quarter chapter suit cruel scrub verify stuff volume control learn dust';
 
+const wallet = {
+  secretKey: 'ba_I1lro/ANfEKuBUal0Glo++D5abkcFLIIihTDLcC8l3My1PuP',
+  chainCode: 'ba_XZL45EKIQiLe9v/pkY37Bn3GiqLXZ5v2hIya6llA0QOlYf6i',
+} as const;
+
 describe('Account mnemonic factory', () => {
+  it('derives wallet', async () => {
+    const factory = new AccountMnemonicFactory(mnemonic);
+    expect(await factory.getWallet()).to.be.eql(wallet);
+  });
+
   it('initializes an account', async () => {
     const factory = new AccountMnemonicFactory(mnemonic);
     const account = await factory.initialize(42);
     expect(account).to.be.instanceOf(MemoryAccount);
+    expect(account.address).to.be.equal('ak_2HteeujaJzutKeFZiAmYTzcagSoRErSXpBFV179xYgqT4teakv');
+  });
+
+  it('initializes an account by wallet', async () => {
+    const factory = new AccountMnemonicFactory(wallet);
+    const account = await factory.initialize(42);
     expect(account.address).to.be.equal('ak_2HteeujaJzutKeFZiAmYTzcagSoRErSXpBFV179xYgqT4teakv');
   });
 
