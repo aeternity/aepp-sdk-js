0b16a325df96b8f879e5372094ef9ce62beae478
Denis Davidyuk <denis_davidyuk@hotmail.com>
2024-08-24 20:32:29 +1000

    feat(middleware): prefixed types provided by OpenApi instead of strings

--- tooling/autorest/postprocessing.mjs
+++ tooling/autorest/postprocessing.mjs
@@ -19,38 +19,60 @@ const middlewareBigIntPropertyNames = [
   'channelReserve', 'initiatorAmount', 'responderAmount', // channels
 ];
 
-const name = process.argv.at(-1);
+function toTsType(types) {
+  const tsTypes = types.split(',').map((type) => {
+    if (type.length === 2) return `\`${type}_\${string}\``;
+    // eslint-disable-next-line no-template-curly-in-string
+    if (type === 'name') return '`${string}.chain`';
+    return type;
+  });
+  return tsTypes.join(' | ');
+}
+
+const module = process.argv.at(-1);
 await Promise.all([
   (async () => {
-    const path = `./src/apis/${name}/${name}.ts`;
+    const path = `./src/apis/${module}/${module}.ts`;
     let content = await fs.promises.readFile(path, { encoding: 'utf-8' });
     content = content.replace(/ {2}\$host: string;/, '  readonly $host: string;');
 
-    if (name === 'node' || name === 'middleware') {
+    if (module === 'node' || module === 'middleware') {
       content = `import { createSerializer } from "../../utils/autorest";\n${content}`;
       content = content.replace('coreClient.createSerializer', 'createSerializer');
     }
 
-    if (name === 'middleware') {
+    if (module === 'middleware') {
       const operationSpecNames = [
         ...content
           .matchAll(/const (\w+OperationSpec): coreClient\.OperationSpec = {/g),
       ].map(([, nm]) => nm);
       content = `${content}\nexport const operationSpecs = [\n  ${operationSpecNames.join(',\n  ')},\n] as const;\n`;
+
+      const parts = content.split('__sdk__');
+      content = parts.shift();
+      while (parts.length) {
+        const idx = content.lastIndexOf('@param ') + '@param '.length;
+        const name = content.slice(idx, idx + content.slice(idx).indexOf(' '));
+        const types = parts.shift();
+        const needle = `${name}: string`;
+        const replaceIdx = parts.findIndex((part) => part.includes(needle));
+        parts[replaceIdx] = parts[replaceIdx].replace(needle, `${name}: ${toTsType(types)}`);
+        content += parts.shift();
+      }
     }
 
     await fs.promises.writeFile(path, content);
   })(),
-  ...name === 'node' || name === 'middleware' ? [(async () => {
-    const path = `./src/apis/${name}/models/index.ts`;
+  ...module === 'node' || module === 'middleware' ? [(async () => {
+    const path = `./src/apis/${module}/models/index.ts`;
     let content = await fs.promises.readFile(path, { encoding: 'utf-8' });
 
-    (name === 'node' ? nodeBigIntPropertyNames : middlewareBigIntPropertyNames)
+    (module === 'node' ? nodeBigIntPropertyNames : middlewareBigIntPropertyNames)
       .forEach((property) => {
         content = content.replaceAll(new RegExp(`(${property}[?]?:) number`, 'g'), '$1 bigint');
       });
 
-    if (name === 'node') {
+    if (module === 'node') {
       /* eslint-disable no-template-curly-in-string */
       content = content.replaceAll(/(txHash\??:) string/g, '$1 `th_${string}`');
       content = content.replaceAll(/(bytecode\??:) string/g, '$1 `cb_${string}`');
@@ -58,7 +80,7 @@ await Promise.all([
       content = content.replaceAll('topics: number[]', 'topics: bigint[]');
     }
 
-    if (name === 'middleware') {
+    if (module === 'middleware') {
       content = `import { MiddlewarePage } from "../../../utils/MiddlewarePage";\n${content}`;
       content = content.replace(/export interface PaginatedResponse {.*?}\n\n/gs, '');
       content = content.replace(
@@ -82,14 +104,21 @@ await Promise.all([
       if (content.includes('PaginatedResponse')) {
         throw new Error('Not all PaginatedResponse instances removed');
       }
+
+      const parts = content.split('__sdk__');
+      content = parts.shift();
+      while (parts.length) {
+        const types = parts.shift();
+        content += parts.shift().replace(': string', `: ${toTsType(types)}`);
+      }
     }
 
     await fs.promises.writeFile(path, content);
   })(), (async () => {
-    const path = `./src/apis/${name}/models/mappers.ts`;
+    const path = `./src/apis/${module}/models/mappers.ts`;
     let content = await fs.promises.readFile(path, { encoding: 'utf-8' });
 
-    (name === 'node' ? nodeBigIntPropertyNames : middlewareBigIntPropertyNames)
+    (module === 'node' ? nodeBigIntPropertyNames : middlewareBigIntPropertyNames)
       .forEach((property) => {
         content = content.replace(
           new RegExp(
@@ -107,7 +136,7 @@ await Promise.all([
         );
       });
 
-    if (name === 'node') {
+    if (module === 'node') {
       content = content.replace(
         /topics: (.+?name: "Sequence".+?)(\s+)name: "Number",/mgs,
         'topics: $1'
